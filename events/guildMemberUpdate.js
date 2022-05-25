fc6c3b6bac8d9fcff842838cebf7b9cb08a66863
TÆMBØ <kuneshr123@gmail.com>
2022-05-24 21:28:17 -0700

    auto-updating embed for DF members

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -6,17 +6,19 @@ module.exports = {
     execute: async (client, member) => {
         const guild = client.guilds.cache.get(client.config.mainServer.id);
         
-        const staff = {
-            moderator: await guild.roles.fetch(client.config.mainServer.roles.mod),
-            trialmoderator: await guild.roles.fetch(client.config.mainServer.roles.minimod),
-            botdev: await guild.roles.fetch(client.config.mainServer.roles.botdeveloper)
+        const df = {
+            dfgreen: await guild.roles.fetch(client.config.mainServer.roles.dfgreen),
+            dfblue: await guild.roles.fetch(client.config.mainServer.roles.dfblue),
+            dforange: await guild.roles.fetch(client.config.mainServer.roles.dforange),
+            dfyellow: await guild.roles.fetch(client.config.mainServer.roles.dfyellow)
         };
-        const mod = await staff.moderator.members.filter(x=>!x.roles.cache.has(client.config.mainServer.roles.admin)).map(e=>`<@${e.user.id}>`).join("\n") || " ";
-        const tm = await staff.trialmoderator.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
-        const bot_dev = await staff.botdev.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
+        const df_green = await df.dfgreen.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
+        const df_blue = await df.dfblue.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
+        const df_orange = await df.dforange.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
+        const df_yellow = await df.dfyellow.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
 
-        client.channels.cache.get('940726714915495946').messages.fetch('970386809781624954').then((msg)=>{ msg.edit({embeds: [new client.embed(msg.embeds[0])
-            .setDescription(`<@&${client.config.mainServer.roles.mod}>: ${staff.moderator.members.filter(x=>!x.roles.cache.has(client.config.mainServer.roles.admin)).size}\n${mod}\n\n<@&${client.config.mainServer.roles.minimod}>: ${staff.trialmoderator.members.size}\n${tm}\n\n<@&${client.config.mainServer.roles.botdeveloper}>: ${staff.botdev.members.size}\n${bot_dev}`)]})})
+        client.channels.cache.get('778848112588095559').messages.fetch('978864042124906547').then((msg)=>{ msg.edit({embeds: [new client.embed(msg.embeds[0])
+            .setDescription(`<@&${client.config.mainServer.roles.dfgreen}>: ${df.dfgreen.members.size}\n${df_green}\n\n<@&${client.config.mainServer.roles.dfblue}>: ${df.dfblue.members.size}\n${df_blue}\n\n<@&${client.config.mainServer.roles.dforange}>: ${df.dforange.members.size}\n${df_orange}\n\n<@&${client.config.mainServer.roles.dfyellow}>: ${df.dfyellow.members.size}\n${df_yellow}`)]})})
         
     }
 }

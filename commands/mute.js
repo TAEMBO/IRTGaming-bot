563e7956493fd5003fd3d1cc128711d025b78f1b
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-05-27 12:52:23 -0700

    disable /mute

--- commands/mute.js
+++ commands/mute.js
@@ -12,4 +12,5 @@ module.exports = {
 		client.punish(client, interaction, 'mute');
 	},
 	data: new SlashCommandBuilder().setName("mute").setDescription("Mutes a member.").addUserOption((opt)=>opt.setName("member").setDescription("The member to mute").setRequired(true)).addStringOption((opt)=>opt.setName("time").setDescription("The time for the mute.")).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for banning the member.").setRequired(false)),
+	disabled: true
 };

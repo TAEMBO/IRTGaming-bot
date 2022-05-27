bd2264c7566181f92384ff271237d401e26e89fa
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-05-27 13:03:16 -0700

    disable /warn

--- commands/warn.js
+++ commands/warn.js
@@ -11,5 +11,6 @@ module.exports = {
 
 		client.punish(client, interaction, 'warn');
 	},
-	data: new SlashCommandBuilder().setName("warn").setDescription("Warns a user.").addUserOption((opt)=>opt.setName("member").setDescription("The member to warn.").setRequired(true)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason to warn the user.").setRequired(false))
+	data: new SlashCommandBuilder().setName("warn").setDescription("Warns a user.").addUserOption((opt)=>opt.setName("member").setDescription("The member to warn.").setRequired(true)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason to warn the user.").setRequired(false)),
+	disabled: true
 };

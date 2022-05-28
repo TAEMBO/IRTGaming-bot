834272b83ab2f7013219307fe4ed99ae54aff266
TÆMBØ <kuneshr123@gmail.com>
2022-05-28 10:57:12 -0700

    fix formatting

--- commands/apply.js
+++ commands/apply.js
@@ -5,7 +5,7 @@ module.exports = {
         const channel = client.channels.resolve('811341461223112714');
 
         interaction.reply({content: 'https://forms.gle/JivS14vJgcJCKigq7', ephemeral: true})
-        channel.send(`<${interaction.user.id}> (${interaction.user.tag}) opened an MP Staff application at ${Math.round(new Date.now() / 1000)}\nForm: https://forms.gle/JivS14vJgcJCKigq7`)
+        channel.send(`<@${interaction.user.id}> (${interaction.user.tag}) opened an MP Staff application at <t:${Math.round(Date.now() / 1000)}>\nForm: <https://forms.gle/JivS14vJgcJCKigq7>`)
     },
     data: new SlashCommandBuilder().setName("apply").setDescription("Apply for MP Staff")
 };

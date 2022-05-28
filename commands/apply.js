a0d8f99e1edd04c3b8cb83f9882fb6d0b3ce2b9c
TÆMBØ <kuneshr123@gmail.com>
2022-05-28 10:45:21 -0700

    Create apply.js

---
+++ commands/apply.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = { 
+    run: async (client, interaction, user) => {
+        const channel = client.channels.resolve('811341461223112714');
+
+        interaction.reply({content: 'https://forms.gle/JivS14vJgcJCKigq7', ephemeral: true})
+        channel.send(`<${interaction.user.id}> (${interaction.user.tag}) opened an MP Staff application at ${Math.round(new Date.now() / 1000)}\nForm: https://forms.gle/JivS14vJgcJCKigq7`)
+    },
+    data: new SlashCommandBuilder().setName("apply").setDescription("Apply for MP Staff")
+};

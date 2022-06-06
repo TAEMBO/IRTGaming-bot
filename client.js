41596cfd056acf4021a3cd1e5a97d353821244e9
TÆMBØ <kuneshr123@gmail.com>
2022-06-05 17:21:05 -0700

    Update client.js

--- client.js
+++ client.js
@@ -135,8 +135,7 @@ class YClient extends Client {
 			channel_log.send(`\`${player.name}\` | \`${FSserver.data.server.name}\` | <t:${Math.round(new Date() / 1000)}>`)
 		}
         })
-        client.channels.resolve(VC).edit({name: `${VCName}`})
-        console.log('a')
+        client.channels.resolve(VC).edit({name: `${FSserver.data.slots.capacity === 0 ? `🔴` : '🟢'} ${VCName} ${FSserver.data.slots.used}/${FSserver.data.slots.capacity}`})
         embed.setAuthor({name: `${FSserver.data.slots.used}/${FSserver.data.slots.capacity}`})
         embed.setTitle(FSserver.data.server.name)
 		if (FSserver.data.slots.used === FSserver.data.slots.capacity) {

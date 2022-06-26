91a348533ed9c2ae98d053c4023a0716054f2b5f
TÆMBØ <kuneshr123@gmail.com>
2022-06-25 20:15:15 -0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,3 +1,5 @@
+const { MessageSelectMenu } = require('discord.js');
+
 module.exports = {
     name: "messageCreate",
     tracker: false,
@@ -266,6 +268,11 @@ module.exports = {
 	}
 	if (message.content.toLowerCase().includes('smoker')) {
 		message.react('🚭')
+	}
+	if (message.content.toLowerCase().includes('no') && message.author.id === '345371418151878657') {
+		const curry = await interaction.guild.members.fetch('345371418151878657');
+		message.delete()
+		curry.send('no saying no')
 	}
 }
 }

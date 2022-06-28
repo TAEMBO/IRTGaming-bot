25a529aa832d2bc31f91b615c43ae05e11bbe156
TÆMBØ <kuneshr123@gmail.com>
2022-06-28 16:48:22 -0700

    add no. of roles in field title

--- commands/whois.js
+++ commands/whois.js
@@ -13,7 +13,7 @@ module.exports = {
 			.addFields(
 			{name: '🔹 Account Creation Date', value: `<t:${Math.round(new Date(member.user.createdTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.user.createdTimestamp) / 1000)}:R>`},
 			{name: '🔹 Join Date', value: `<t:${Math.round(new Date(member.joinedTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.joinedTimestamp) / 1000)}:R>`},
-			{name: '🔹 Roles', value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'})
+			{name: `🔹 Roles: ${member.roles.cache.size - 1}`, value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'})
 			.setColor(member.displayColor || client.config.embedColor)
 			.setImage(member.user.bannerURL({ format: 'png', dynamic: true, size: 1024}))
 			if (member.user.id === interaction.guild.ownerId) embed.setDescription(`<@${member.user.id}>\n\`${member.user.id}\`\n__**Server Owner**__ <:crown:945149846506078250>`)

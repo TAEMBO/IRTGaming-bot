ad3b6ff9876b15d2a8cb10c2b3e8d9ddf35fde81
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-06-28 10:46:11 -0700

    add createTable function

--- client.js
+++ client.js
@@ -192,6 +192,40 @@ class YClient extends Client {
             
         }, 55000); 
     }
+    createTable(columnTitles = [], rowsData = [], options = {}, client) {
+        const rows = [];
+        // { columnAlign: [], columnSeparator: [], columnEmptyChar: [] }
+        let { columnAlign = [], columnSeparator = [], columnEmptyChar = [] } = options;
+        if (columnSeparator.length < 1) columnSeparator.push('|');
+        columnSeparator = columnSeparator.map(x => ' ' + x + ' ');
+        // column widths
+        const columnWidths = columnTitles.map((title, i) => Math.max(title.length, ...rowsData.map(x => x[i].length)));
+        // first row
+        rows.push(columnTitles.map((title, i) => {
+            let text = client.alignText(title, columnWidths[i], columnAlign[i], columnEmptyChar[i]);
+            if (columnSeparator[i]) {
+                text += ' '.repeat(columnSeparator[i].length);
+            }
+            return text;
+        }).join(''));
+        // big line
+        rows.push('━'.repeat(rows[0].length));
+        // data
+        // remove unicode
+        rowsData.map(row => {
+            return row.map(element => {
+                return element.split('').map(char => {
+                    if (char.charCodeAt(0) > 128) return '□';
+                }).join('');
+            });
+        });
+        rows.push(rowsData.map(row => row.map((element, i) => {
+                return client.alignText(element, columnWidths[i], columnAlign[i], columnEmptyChar[i]) + (i === columnTitles.length - 1 ? '' : columnSeparator[i]);
+            }).join('')
+        ).join('\n'))
+    
+        return rows.join('\n');
+    }
     removeCustomValue(array, value){
         for(let i = 0; i < array.length; i++){
             if(array[i].includes(value)){

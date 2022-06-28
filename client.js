bd1a3960b497b6e9cafa25a539d2834cb07acf43
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-06-28 10:52:32 -0700

    add alignText function

--- client.js
+++ client.js
@@ -192,6 +192,17 @@ class YClient extends Client {
             
         }, 55000); 
     }
+    alignText(text, length, alignment, emptyChar = ' ') {
+        if (alignment === 'right') {
+            text = emptyChar.repeat(length - text.length) + text;
+        } else if (alignment === 'middle') {
+            const emptyCharsPerSide = (length - text.length) / 2;
+            text = emptyChar.repeat(Math.floor(emptyCharsPerSide)) + text + emptyChar.repeat(Math.floor(emptyCharsPerSide));
+        } else {
+            text = text + emptyChar.repeat(length - text.length);
+        }
+        return text;
+    }
     createTable(columnTitles = [], rowsData = [], options = {}, client) {
         const rows = [];
         // { columnAlign: [], columnSeparator: [], columnEmptyChar: [] }

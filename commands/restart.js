9a3c4067a5bab72cb811ca6acbef8ae7714f1939
TÆMBØ <kuneshr123@gmail.com>
2022-06-29 13:12:36 -0700

    edit restart and update logs

--- commands/restart.js
+++ commands/restart.js
@@ -3,7 +3,7 @@ module.exports = {
   run: (client, interaction) => {
     if (!client.config.eval.whitelist.includes(interaction.user.id)) return interaction.reply({content: `You're not allowed to use this command.`, allowedMentions: {roles: false}});
     interaction.reply("Restarting...");
-    client.channels.cache.get(client.config.mainServer.channels.modlogs).send({embeds: [new client.embed().setDescription(`**Restart issued by ${interaction.user.tag}** \`${interaction.user.id}\``).setColor(client.config.embedColor).setTimestamp()]}).then(async ()=> eval(process.exit(-1)))
+    client.channels.cache.get(client.config.mainServer.channels.modlogs).send(`**Restart issued by ${interaction.user.tag}** \`${interaction.user.id}\``).then(async ()=> eval(process.exit(-1)))
   },
   data: new SlashCommandBuilder().setName("restart").setDescription("Restarts the bot.")
 };
--- commands/update.js
+++ commands/update.js
@@ -3,16 +3,18 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
     run: async (client, interaction) => {
         if (!client.config.eval.whitelist.includes(interaction.user.id)) return interaction.reply({content: `You're not allowed to use this command.`})
-        const msg = await interaction.reply({content: "Pulling from repo...", allowedMentions: {repliedUser: false}, fetchReply: true});
+        const msg = await interaction.reply({content: "Pulling from repo...", fetchReply: true});
         require("child_process").exec("git pull")
 
         const options = interaction.options.getString('options');
 
         switch (options) {
           case 'ur':
+            client.channels.cache.get(client.config.mainServer.channels.modlogs).send(`**Update and restart issued by ${interaction.user.tag}** \`${interaction.user.id}\``)
             msg.edit({content: "Restarting..."}).then(async ()=> eval(process.exit(-1)));
             return
           case 'u':
+            client.channels.cache.get(client.config.mainServer.channels.modlogs).send(`**Update issued by ${interaction.user.tag}** \`${interaction.user.id}\``)
             msg.edit({content: "Pulled from repo"});
             return;
         }

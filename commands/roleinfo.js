d51fa7fb301667bf9c1e3705d6f190c0ee8be67e
TÆMBØ <kuneshr123@gmail.com>
2022-07-06 10:15:02 -0700

    sus bug

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -9,9 +9,9 @@ module.exports = {
 		const embed = new client.embed()
 			.setTitle('Role Info: ' + role.name)
 			.addFields(
-			{name: '🔹 Id', value: role.id},
-			{name: '🔹 Color', value: `\`${role.hexColor}\``},
-			{name: '🔹 Creation Date', value: `<t:${Math.round(new Date(role.createdTimestamp) / 1000)}>\n<t:${Math.round(new Date(role.createdTimestamp) / 1000)}:R>`},
+			{name: '🔹 Id', value: `\`${role.id}\` `},
+			{name: '🔹 Color', value: `\`${role.hexColor}\` `},
+			{name: '🔹 Creation Date', value: `<t:${Math.round(new Date(role.createdTimestamp) / 1000)}>\n<t:${Math.round(new Date(role.createdTimestamp) / 1000)}:R> `},
 			{name: '🔹 Misc', value: `Hoist: \`${role.hoist}\`\nMentionable: \`${role.mentionable}\`\nPosition: \`${role.position}\` from bottom\nMembers: \`${role.members.size}\`\n${role.members.size < 11 ? Role : ''}`},
 			{name: '🔹 Key Permissions', value: (permissions.includes('ADMINISTRATOR') ? ['ADMINISTRATOR'] : permissions.filter(x => keyPermissions.includes(x))).map(x => {    return x.split('_').map((y, i) => i === 0 ? y[0] + y.slice(1).toLowerCase() : y.toLowerCase()).join(' ')}).join(', ') || ''})
 			.setColor(role.color || '#fefefe')

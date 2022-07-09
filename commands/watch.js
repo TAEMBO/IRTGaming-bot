004315ddd5ba043b2bca84fd2fafa578424ae13d
TÆMBØ <kuneshr123@gmail.com>
2022-07-09 09:15:08 -0700

    add filter to prevent duplicates

--- commands/watch.js
+++ commands/watch.js
@@ -5,8 +5,9 @@ module.exports = {
         if (!client.isMPStaff(client, interaction.member)) return interaction.reply({content: `You need the <@&${client.config.mainServer.roles.mpstaff}> role to use this command`, allowedMentions: {roles: false}});
 
         const name = interaction.options.getString("username");
+        if (client.watchList._content.includes(name)) return interaction.reply('That username is already on the watchList');
         client.watchList.addData(name).forceSave();
-        interaction.reply({content: `Successfully added \`${name}\``})
+        interaction.reply({content: `Successfully added \`${name}\``});
     },
     data: new SlashCommandBuilder()
         .setName("watch")

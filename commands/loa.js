5af527c2edf93fa578c3d928503fcc86602d4a79
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-07-18 11:29:48 -0700

    add catch for failed nickname set

--- commands/loa.js
+++ commands/loa.js
@@ -6,12 +6,12 @@ module.exports = {
         if (!interaction.member.roles.cache.has(client.config.mainServer.roles.loa)) {
             interaction.member.roles.add(client.config.mainServer.roles.loa);
             setTimeout(() => interaction.member.roles.remove(client.config.mainServer.roles.mpstaff), 500);
-            setTimeout(() => interaction.member.setNickname(`[LOA] ${interaction.member.nickname}`), 1000);
+            setTimeout(() => interaction.member.setNickname(`[LOA] ${interaction.member.nickname}`).catch((e) => console.log('failed to set nickname for LOA')), 1000);
             setTimeout(() => interaction.reply({content: 'LOA status set', ephemeral: true}), 1500);
         } else {
             interaction.member.roles.add(client.config.mainServer.roles.mpstaff);
             setTimeout(() => interaction.member.roles.remove(client.config.mainServer.roles.loa), 500);
-            setTimeout(() => interaction.member.setNickname(`${interaction.member.nickname.replaceAll('[LOA] ', '')}`), 1000);
+            setTimeout(() => interaction.member.setNickname(`${interaction.member.nickname.replaceAll('[LOA] ', '')}`).catch((e) => console.log('failed to set nickname for LOA')), 1000);
             setTimeout(() => interaction.reply({content: 'LOA status removed', ephemeral: true}), 1500);
         }
 	},

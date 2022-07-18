8cf5b697240b0acde1af5eb678fdfabe716791ad
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-07-18 08:38:44 -0700

    attempt to fix roles not being given

--- commands/loa.js
+++ commands/loa.js
@@ -5,14 +5,14 @@ module.exports = {
         if (!client.isMPStaff(client, interaction.member)) return interaction.reply({content: `You need the <@&${client.config.mainServer.roles.mpstaff}> role to use this command`, allowedMentions: {roles: false}})
         if (!interaction.member.roles.cache.has(client.config.mainServer.roles.loa)) {
             interaction.member.roles.add(client.config.mainServer.roles.loa);
-            interaction.member.roles.remove(client.config.mainServer.roles.mpstaff);
-            interaction.member.setNickname(`[LOA] ${interaction.member.nickname}`);
-            interaction.reply({content: 'LOA status set', ephemeral: true});
+            setTimeout(() => interaction.member.roles.remove(client.config.mainServer.roles.mpstaff), 500);
+            setTimeout(() => interaction.member.setNickname(`[LOA] ${interaction.member.nickname}`), 1000);
+            setTimeout(() => interaction.reply({content: 'LOA status set', ephemeral: true}), 1500);
         } else {
             interaction.member.roles.add(client.config.mainServer.roles.mpstaff);
-            interaction.member.roles.remove(client.config.mainServer.roles.loa);
-            interaction.member.setNickname(`${interaction.member.nickname.replaceAll('[LOA] ', '')}`);
-            interaction.reply({content: 'LOA status removed', ephemeral: true});
+            setTimeout(() => interaction.member.roles.remove(client.config.mainServer.roles.loa), 500);
+            setTimeout(() => interaction.member.setNickname(`${interaction.member.nickname.replaceAll('[LOA] ', '')}`), 1000);
+            setTimeout(() => interaction.reply({content: 'LOA status removed', ephemeral: true}), 1500);
         }
 	},
 	data: new SlashCommandBuilder()

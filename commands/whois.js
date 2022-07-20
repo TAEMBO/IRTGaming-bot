261443a0712898c56b84fd17a20ed7ba45a062c6
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-07-20 14:40:02 -0700

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -18,8 +18,7 @@ module.exports = {
 			.addFields(
 			{name: '🔹 Account Creation Date', value: `<t:${Math.round(new Date(member.user.createdTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.user.createdTimestamp) / 1000)}:R>`},
 			{name: '🔹 Join Date', value: `<t:${Math.round(new Date(member.joinedTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.joinedTimestamp) / 1000)}:R>`},
-			{name: `🔹 Roles: ${member.roles.cache.size - 1}`, value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'},
-			{name: `🔹 Status: ${member.presence.status}`, value:`${member.presence.status === 'offline' ? 'N/A' : `Web: ${member.presence.clientStatus.web ? member.presence.clientStatus.web : 'offline'}\nMobile: ${member.presence.clientStatus.mobile ? member.presence.clientStatus.mobile : 'offline'}\nDesktop: ${member.presence.clientStatus.desktop ? member.presence.clientStatus.desktop : 'offline'}`}`})
+			{name: `🔹 Roles: ${member.roles.cache.size - 1}`, value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'})
 			.setColor(member.displayColor || client.config.embedColor)
 			.setImage(member.user.bannerURL({ format: 'png', dynamic: true, size: 1024}))
 			if (member.premiumSinceTimestamp !== null) {
@@ -27,7 +26,7 @@ module.exports = {
 					{name: '🔹 Server Boosting Since', value: `<t:${Math.round(new Date(member.premiumSinceTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.premiumSinceTimestamp) / 1000)}:R>`}
 				)
 			}
-			if (member.presence.status) {
+			if (member.presence) {
 				embed.addFields(
 					{name: `🔹 Status: ${member.presence.status}`, value:`${member.presence.status === 'offline' ? 'N/A' : `Web: ${member.presence.clientStatus.web ? member.presence.clientStatus.web : 'offline'}\nMobile: ${member.presence.clientStatus.mobile ? member.presence.clientStatus.mobile : 'offline'}\nDesktop: ${member.presence.clientStatus.desktop ? member.presence.clientStatus.desktop : 'offline'}`}`}
 				)

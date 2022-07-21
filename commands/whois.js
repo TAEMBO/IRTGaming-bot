71a604037ce1b1112f74b7f8c99105a4701ce4a9
TÆMBØ <kuneshr123@gmail.com>
2022-07-21 09:55:12 -0700

    use emojis for status

--- commands/whois.js
+++ commands/whois.js
@@ -9,13 +9,13 @@ function Status(client, activities) {
 function convert(status) {
 switch (status) {
 case "offline":
-"⚫";
+return "⚫";
 case "idle":
-"🟡";
+return "🟡";
 case "dnd":
-"🔴";
+return "🔴";
 case "online":
-"🟢";
+return "🟢";
 }
 }
 module.exports = {
@@ -35,12 +35,12 @@ module.exports = {
 			.setImage(member.user.bannerURL({ format: 'png', dynamic: true, size: 1024}))
 			if (member.premiumSinceTimestamp !== null) {
 				embed.addFields(
-					{name: '🔹 Server Boosting Since', value: `<t:${Math.round(new Date(member.premiumSinceTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.premiumSinceTimestamp) / 1000)}:R>`}
+					{name: '🔹 Server Boosting Since', value: `<t:${Math.round(new Date(member.premiumSinceTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.premiumSinceTimestamp) / 1000)}:R>`, inline: true}
 				)
 			}
 			if (member.presence) {
 				embed.addFields(
-					{name: `🔹 Status: ${member.presence.status}`, value:`${member.presence.status === 'offline' ? 'N/A' : `Web: ${member.presence.clientStatus.web ? member.presence.clientStatus.web : 'offline'}\nMobile: ${member.presence.clientStatus.mobile ? member.presence.clientStatus.mobile : 'offline'}\nDesktop: ${member.presence.clientStatus.desktop ? member.presence.clientStatus.desktop : 'offline'}`}`}
+					{name: `🔹 Status: ${member.presence.status}`, value:`${member.presence.status === 'offline' ? 'N/A' : `Web: ${member.presence.clientStatus.web ? convert(member.presence.clientStatus.web) : convert('offline')}\nMobile: ${member.presence.clientStatus.mobile ? convert(member.presence.clientStatus.mobile) : convert('offline')}\nDesktop: ${member.presence.clientStatus.desktop ? convert(member.presence.clientStatus.desktop) : convert('offline')}`}`, inline: true}
 				)
 			}
 		interaction.reply({embeds: [embed]});

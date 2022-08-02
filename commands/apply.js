539d78045a3fb5ac001d9f324e3e8dde4d1672e5
TÆMBØ <kuneshr123@gmail.com>
2022-08-02 11:06:17 -0700

    forgor to return

--- commands/apply.js
+++ commands/apply.js
@@ -6,6 +6,7 @@ module.exports = {
         if ((Date.now() - interaction.member.joinedTimestamp) < 604800000) {
             interaction.reply({content: 'You must be on the Discord server for at least two weeks before applying.', ephemeral: true})
             channel.send(`<@${interaction.user.id}> (${interaction.user.tag}) tried to open an MP Staff application but was rejected due to insufficient time.`)
+            return;
         }
 
         interaction.reply({content: 'https://forms.gle/JivS14vJgcJCKigq7', ephemeral: true})

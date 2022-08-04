e4be51b1776d4706b86d06fe05ab2a245e2de217
TÆMBØ <kuneshr123@gmail.com>
2022-08-03 22:18:08 -0700

    Update client.js

--- client.js
+++ client.js
@@ -110,7 +110,7 @@ class YClient extends Client {
         const PGdata = require('./databases/PGPlayerData.json')
         const PSdata = require('./databases/PSPlayerData.json')
         const MFdata = require('./databases/MFPlayerData.json')
-        const Whitelist = ["Bernie", "RedbaD", "SpongeBoi69", "Kazmerev", "Hungarian__0101", "Bacon"]
+        const Whitelist = ["Bernie", "RedbaD", "SpongeBoi69", "Kazmerev", "Hungarian__0101", "Bacon", "Sersha", "Helper B", "777Stupid"]
         const wlChannel = client.channels.resolve(client.config.mainServer.channels.watchlist);
         const logChannel = client.channels.resolve(client.config.mainServer.channels.fslogs)
         const playerInfo = [];

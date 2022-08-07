7eb9f03e16fcb56d600b20f4634b247d59528a87
TÆMBØ <kuneshr123@gmail.com>
2022-08-07 11:11:54 -0700

    Update addbannedword.js

--- commands/addbannedword.js
+++ commands/addbannedword.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, Faces } = require("@discordjs/builders");
 
 module.exports = {
 	run: (client, interaction) => {
-		if (!client.hasModPerms(client, interaction.member)) return interaction.reply({content: client.yOuNeEdMoD, allowedMentions: {roles: false}});
+		if (!client.hasModPerms(client, interaction.member) && interaction.user.id !== '615761944154210305') return interaction.reply({content: client.yOuNeEdMoD, allowedMentions: {roles: false}});
 		const word = interaction.options.getString("word")
 		if (client.bannedWords._content.includes(word)) return interaction.reply('That word is already added.')
 		client.bannedWords.addData(word).forceSave();

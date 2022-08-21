2c2d7f67e0640e901675d8e6da3889ffc0be06fe
TÆMBØ <kuneshr123@gmail.com>
2022-08-21 16:48:55 -0700

    Fix role perms listing

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -3,7 +3,7 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = {
 	run: (client, interaction) => {
 		const role = interaction.options.getRole("role");
-		const keyPermissions = ['ADMINISTRATOR', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'MANAGE_GUILD', 'VIEW_AUDIT_LOG', 'MANAGE_MESSAGES', 'MENTION_EVERYONE', 'USE_EXTERNAL_EMOJIS', 'MANAGE_ROLES', 'MANAGE_EMOJIS_AND_STICKERS', 'MODERATE_MEMBERS'];
+		const keyPermissions = ['Administrator', 'KickMembers', 'BanMembers', 'ManageChannels', 'ManageGuild', 'ViewAuditLog', 'ManageMessages', 'MentionEveryone', 'UseExternalEmojis', 'ManageRoles', 'ManageEmojisAndStickers', 'ModerateMembers'];
 		const permissions = role.permissions.toArray();
 		const Role = role.members.map(e=>`**${e.user.tag}**`).join("\n") || "";
 		const embed = new client.embed()
@@ -13,7 +13,7 @@ module.exports = {
 			{name: '🔹 Color', value: `\`${role.hexColor}\``, inline: true},
 			{name: '🔹 Creation Date', value: `<t:${Math.round(new Date(role.createdTimestamp) / 1000)}>\n<t:${Math.round(new Date(role.createdTimestamp) / 1000)}:R> `, inline: true},
 			{name: '🔹 Misc', value: `Hoist: \`${role.hoist}\`\nMentionable: \`${role.mentionable}\`\nPosition: \`${role.position}\` from bottom\nMembers: \`${role.members.size}\`\n${role.members.size < 11 ? Role : ''}`, inline: true},
-			{name: '🔹 Key Permissions', value: (permissions.includes('ADMINISTRATOR') ? ['ADMINISTRATOR'] : permissions.filter(x => keyPermissions.includes(x))).map(x => {    return x.split('_').map((y, i) => i === 0 ? y[0] + y.slice(1).toLowerCase() : y.toLowerCase()).join(' ')}).join(', ') || 'None', inline: true})
+			{name: '🔹 Key Permissions', value: (permissions.includes('Administrator') ? ['Administrator'] : permissions.filter(x => keyPermissions.includes(x))).map(x => {    return x.split('_').map((y, i) => y).join(' ')}).join(', ') || 'None', inline: true})
 			.setColor(role.color || '#fefefe')
 			.setThumbnail(role?.iconURL())
 		interaction.reply({embeds: [embed]});

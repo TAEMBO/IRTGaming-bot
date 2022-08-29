bc1822af26810df1ee30fcc6181844b41512de44
TÆMBØ <kuneshr123@gmail.com>
2022-08-28 21:21:34 -0700

    Update client.js

--- client.js
+++ client.js
@@ -124,7 +124,7 @@ class YClient extends Client {
         const PGdata = require('./databases/PGPlayerData.json')
         const PSdata = require('./databases/PSPlayerData.json')
         const MFdata = require('./databases/MFPlayerData.json')
-        const Whitelist = ["Bernie", "RedbaD", "SpongeBoi69", "Kazmerev", "Hungarian__0101", "Bacon", "Sersha", "Helper B", "777Stupid", "Andyk1978", "Andrewk1978", "OmgxBeckyx"]
+        const Whitelist = ["Bernie", "RedbaD", "SpongeBoi69", "Kazmerev", "Hungarian__0101", "Bacon", "Sersha", "Helper B", "777Stupid", "Andyk1978", "Andrewk1978", "OmgxBeckyx", "Bumpi"]
         const wlPing = ["263724396672188417", "769710040596217897", "642735886953611265"];
         const wlChannel = client.channels.resolve(client.config.mainServer.channels.watchlist);
         const logChannel = client.channels.resolve(client.config.mainServer.channels.fslogs)
@@ -151,7 +151,7 @@ class YClient extends Client {
             error = true;
             console.log(`[${moment().format('HH:mm:ss')}] ${serverAcro} csg fail`)
         }
-        FScsg = await xjs.xml2js(xml.data, {compact: true, spaces: 2}).careerSavegame.catch((err) => error = true);
+        FScsg = await xjs.xml2js(xml.data, {compact: true, spaces: 2}).careerSavegame;
 
         if (error) {
             embed.setTitle('Host not responding');

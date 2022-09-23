562a44f1ab6bc8cb680bb2d7a24b0cf36e99f04c
TÆMBØ <kuneshr123@gmail.com>
2022-09-22 17:39:40 -0700

    Remove unneeded log

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,6 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = {
   run: (client, interaction) => {
     if (!client.config.eval.whitelist.includes(interaction.user.id)) return interaction.reply(`You're not allowed to use this command.`);
-    client.channels.cache.get(client.config.mainServer.channels.modlogs).send(`**Restart issued by ${interaction.user.tag}**`)
     interaction.reply("Restarting...").then(async ()=> eval(process.exit(-1)))
   },
   data: new SlashCommandBuilder()

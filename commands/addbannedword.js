452cb84f09fefa1664a121f7022dd6f52674f20b
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-10-02 17:39:22 -0700

    Fix broken response

--- commands/addbannedword.js
+++ commands/addbannedword.js
@@ -2,7 +2,7 @@ const {SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
 	run: (client, interaction) => {
-		if (!client.hasModPerms(client, interaction.member) && interaction.user.id !== '615761944154210305') return interaction.reply({content: client.yOuNeEdMoD, allowedMentions: {roles: false}});
+		if (!client.hasModPerms(client, interaction.member) && !client.config.devWhitelist.includes(interaction.member.id)) return client.yOuNeEdMoD(client, interaction);
 		const word = interaction.options.getString("word")
 		if (client.bannedWords._content.includes(word)) return interaction.reply('That word is already added.')
 		client.bannedWords.addData(word).forceSave();

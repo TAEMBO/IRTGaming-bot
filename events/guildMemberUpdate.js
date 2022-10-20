f33ddfb0f9cb6e357bdf4e1c4432ee46802dd464
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-10-19 21:58:29 -0400

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,10 +1,7 @@
 module.exports = {
     name: "guildMemberUpdate",
     execute: async (client, member) => {
-        const tf = {
-            trusted: await client.guilds.cache.get(client.config.mainServer.id).roles.fetch(client.config.mainServer.roles.trustedfarmer)
-        };
-        const mp_tf = await tf.trusted.members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
-        client.channels.cache.get('718555644801712200').messages.fetch('980240957167521863').then((msg)=>{msg.edit({content: `<@&${client.config.mainServer.roles.trustedfarmer}>: ${tf.trusted.members.size}\n${mp_tf}`, allowedMentions: {roles: false}})})
+        const mp_tf = (await client.guilds.cache.get(client.config.mainServer.id).roles.fetch(client.config.mainServer.roles.trustedfarmer)).members.map(e=>`<@${e.user.id}>`).join("\n") || " ";
+        client.channels.cache.get('718555644801712200')?.messages?.fetch('980240957167521863').then((msg)=>{msg.edit({content: `<@&${client.config.mainServer.roles.trustedfarmer}>: ${tf.trusted.members.size}\n${mp_tf}`, allowedMentions: {roles: false}})})
     }
 }

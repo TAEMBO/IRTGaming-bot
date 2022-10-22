be66f1383b4b79ecab8c71e52041a4e0e99b9b8b
TÆMBØ <kuneshr123@gmail.com>
2022-10-21 21:28:19 -0700

    Add GS

--- commands/stats.js
+++ commands/stats.js
@@ -230,6 +230,7 @@ module.exports = {
             let PS;
             let PG;
             let MF;
+            let GS;
             
             try {
                 PS = await client.axios.get(client.tokens.ps.dss, {timeout: 3000});
@@ -246,6 +247,11 @@ module.exports = {
             } catch (err) {
                 console.log(`\x1b[31mstats all; MF failed`)
             }
+            try {
+                GS = await client.axios.get(client.tokens.test.dss, {timeout: 3000});
+            } catch (err) {
+                console.log(`\x1b[31mstats all; GS failed`)
+            }
             const totalCount = [];
             const embed = new client.embed()
                 .setColor(client.config.embedColor)
@@ -257,6 +263,9 @@ module.exports = {
                 }
                 if (MF) {
                     await FSstatsAll(client, MF, embed, totalCount)
+                }
+                if (GS) {
+                    await FSstatsAll(client, GS, embed, totalCount)
                 }
 
                 let sum;
@@ -276,6 +285,8 @@ module.exports = {
             FSstats(client, interaction, client.tokens.pg.dss, 'PGPlayerData');
         } else if (subCmd === 'mf') {
             FSstats(client, interaction, client.tokens.mf.dss, 'MFPlayerData');
+        } else if (subCmd === 'gs') {
+            FSstats(client, interaction, client.tokens.test.dss, 'GSPlayerData');
         } else if (subCmd === 'playertimes') {
             const embed = new client.embed()
                 .setDescription(`Top 20 players with the most time spent on IRTGaming FS22 servers since\n<t:1664645628>\n\n${Object.entries(client.playerTimes._content).sort((a, b) => b[1] - a[1]).slice(0, 20).map((x, i) => `**${i + 1}.** \`${x[0]}\`${(client.FMstaff._content.includes(x[0]) ? ':farmer:' : '')}${(client.TFstaff._content.includes(x[0]) ? ':angel:' : '')}: ${client.formatTime((x[1]*60*1000), 3, { commas: true, longNames: false })}`).join('\n')}`)
@@ -314,6 +325,10 @@ module.exports = {
     .addSubcommand((optt)=>optt
         .setName("mf")
         .setDescription("Multi Farm server stats")
+    )
+    .addSubcommand((optt)=>optt
+        .setName("gs")
+        .setDescription("Grass Silage server stats")
     )
     .addSubcommand((optt)=>optt
         .setName("playertimes")

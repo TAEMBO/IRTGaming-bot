c3602d3fa0b4d28473f27d21d0d412bcb8aaed2d
TÆMBØ <kuneshr123@gmail.com>
2022-10-26 16:24:42 -0700

    Switch Bot and Websocket ordering

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = { 
     run: async (client, interaction) => {
         const msg = await interaction.reply({content: "Pinging...", fetchReply: true});
-        const time = msg.createdTimestamp - interaction.createdTimestamp; msg.edit({content: `Bot: \`${time}\`ms\nWebsocket: \`${client.ws.ping}\`ms`});
+        const time = msg.createdTimestamp - interaction.createdTimestamp; msg.edit({content: `Websocket: \`${client.ws.ping}\`ms\nBot: \`${time}\`ms`});
     },
     data: new SlashCommandBuilder().setName("ping").setDescription("Get the bot's latency")
 };

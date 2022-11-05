32a489c649aedee9a2a639ec95d5a3da5bffbaea
TÆMBØ <kuneshr123@gmail.com>
2022-11-05 16:55:52 -0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,10 +3,10 @@ module.exports = {
     execute: async (client, interaction) => {
         if (interaction.isCommand()) {
             const commandFile = client.commands.get(interaction.commandName);
+            console.log(`\x1b[36m[${client.moment().format('HH:mm:ss')}]`, `\x1b[32m${interaction.user.tag}\x1b[36m used \x1b[32m/${interaction.commandName} ${interaction.options._subcommand ?? ''}\x1b[36m in \x1b[32m#${interaction.channel.name}`);
             if (!client.config.botSwitches.commands && !client.config.devWhitelist.includes(interaction.user.id)) return interaction.reply({content: 'Commands are currently disabled.', ephemeral: true});
             if (commandFile) {
                 if (commandFile.disabled) return interaction.reply({content: 'This command is disabled.', ephemeral: true});
-                console.log(`\x1b[36m[${client.moment().format('HH:mm:ss')}]`, `\x1b[32m${interaction.user.tag}\x1b[36m used \x1b[32m/${interaction.commandName} ${interaction.options._subcommand ?? ''}\x1b[36m in \x1b[32m#${interaction.channel.name}`);
                 try {
                     commandFile.run(client, interaction);
                     commandFile.uses ? commandFile.uses++ : commandFile.uses = 1;

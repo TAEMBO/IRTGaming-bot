30ea251ce50748c03fa3a1878aa422a98a0494d7
TÆMBØ <kuneshr123@gmail.com>
2022-11-29 18:43:56 -0800

    Change to deferReply for bigger graphs

--- commands/stats.ts
+++ commands/stats.ts
@@ -23,6 +23,7 @@ async function FSstatsAll(client: YClient, serverName: FSdss_serverName, embed: 
 }
 
 async function FSstats(client: YClient, interaction: Discord.CommandInteraction, serverName: string, DBName: string) {
+    interaction.deferReply();
 
     const embed = new client.embed()
     const playerInfo: Array<string> = [];
@@ -209,7 +210,7 @@ async function FSstats(client: YClient, interaction: Discord.CommandInteraction,
     embed.setImage('attachment://FSStats.png')
     embed.setColor(Color)
     embed.setDescription(FSserver.data.slots.used === 0 ? '*No players online*' : playerInfo.join("\n"));
-    interaction.reply({embeds: [embed], files: [Image]})
+    interaction.editReply({embeds: [embed], files: [Image]})
 }
 
 export default {

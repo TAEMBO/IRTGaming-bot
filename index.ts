39aecd204783662df041e4ec18f2d72bdc15faa5
TÆMBØ <kuneshr123@gmail.com>
2022-12-05 16:19:45 -0800

    Fix error spam

--- index.ts
+++ index.ts
@@ -41,7 +41,7 @@ client.on("ready", async () => {
 // error handlers
 function logError(error: Error) {
 	console.log(error);                    // vvv I'm well aware my internet is bad, I don't need my own bot to rub it in
-	if (client.config.botSwitches.errorNotify || !error.message.includes('getaddrinfo ENOTFOUND discord.com')) (client.channels.resolve(client.config.mainServer.channels.testing_zone) as Discord.TextChannel).send({content: `<@${client.config.devWhitelist[0]}>`, embeds: [new client.embed().setTitle("Error Caught!").setColor("#420420").setDescription(`**Error:** \`${error.message}\`\n\n**Stack:** \`${`${error.stack}`.slice(0, 2500)}\``)]});
+	if (client.config.botSwitches.errorNotify && !error.message.includes('getaddrinfo ENOTFOUND discord.com')) (client.channels.resolve(client.config.mainServer.channels.testing_zone) as Discord.TextChannel).send({content: `<@${client.config.devWhitelist[0]}>`, embeds: [new client.embed().setTitle("Error Caught!").setColor("#420420").setDescription(`**Error:** \`${error.message}\`\n\n**Stack:** \`${`${error.stack}`.slice(0, 2500)}\``)]});
 }
 process.on('unhandledRejection', async (error: Error) => {
 	logError(error);

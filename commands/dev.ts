f6c7e0f7517494d6d880b84acfd652385a68a0cf
TÆMBØ <kuneshr123@gmail.com>
2022-12-18 16:50:43 -0800

    Update dev.ts

--- commands/dev.ts
+++ commands/dev.ts
@@ -51,7 +51,7 @@ export default {
 				} else {
 					output = '\n' + String(output);
 				}
-				[client.token, client.tokens.ps.login, client.tokens.pg.login, client.tokens.mf.login, client.tokens.ftp.ps, client.tokens.ftp.pg].forEach((login) => {
+				[client.token, client.tokens.ps.login, client.tokens.pg.login, client.tokens.mf.login, client.tokens.ftp.ps.password, client.tokens.ftp.pg.password].forEach((login) => {
 					output = output.replace(login as string, 'LOGIN_LEAK');
 				});
 				const embed = new client.embed()

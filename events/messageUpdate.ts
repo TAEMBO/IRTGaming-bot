d3fce259659be664cd29f871a2588707d86b10d1
TÆMBØ <kuneshr123@gmail.com>
2022-12-26 20:51:32 -0800

    Update messageUpdate.ts

--- events/messageUpdate.ts
+++ events/messageUpdate.ts
@@ -4,8 +4,6 @@ import YClient from "../client";
 export default {
     name: "messageUpdate",
     execute: async (client: YClient, oldMsg: Discord.Message, newMsg: Discord.Message) => {
-        await oldMsg.fetch();
-        await newMsg.fetch();
         if (!client.config.botSwitches.logs || newMsg.author.bot || oldMsg.partial || newMsg.partial || !oldMsg.member || oldMsg.content.length == 0 || newMsg.content == oldMsg.content || newMsg.channel.id == '979863373439184966') return;
         const logChannel = client.channels.resolve(client.config.mainServer.channels.botlogs) as Discord.TextChannel;
         const msgarr = newMsg.content.toLowerCase().split(' ');

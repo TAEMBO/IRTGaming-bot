f3ff95138f905e9343b56d218aab535c296e3193
TÆMBØ <kuneshr123@gmail.com>
2023-01-01 17:02:35 -0800

    Expose the failed attempts, and why is this here

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -14,9 +14,8 @@ export default {
         if (interaction.isCommand()) {
             const commandFile = client.commands.get(interaction.commandName);
             console.log(`[${client.moment().format('HH:mm:ss')}]`, `${interaction.user.tag} used /${interaction.commandName} ${subCmd} in #${interaction.channel.name}`);
-            if (!client.config.botSwitches.commands && !client.config.devWhitelist.includes(interaction.user.id)) return interaction.reply({content: 'Commands are currently disabled.', ephemeral: true});
+            if (!client.config.botSwitches.commands && !client.config.devWhitelist.includes(interaction.user.id)) return interaction.reply('Commands are currently disabled.');
             if (commandFile) {
-                if (commandFile.disabled) return interaction.reply({content: 'This command is disabled.', ephemeral: true});
                 try {
                     commandFile.default.run(client, interaction);
                     commandFile.uses ? commandFile.uses++ : commandFile.uses = 1;

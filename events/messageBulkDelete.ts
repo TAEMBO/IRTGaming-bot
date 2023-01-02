ce6cf4b8c500a65522fa49c5a028deeb75d38ef3
TÆMBØ <kuneshr123@gmail.com>
2023-01-01 16:16:45 -0800

    Map the messages in a simpler way

--- events/messageBulkDelete.ts
+++ events/messageBulkDelete.ts
@@ -3,22 +3,16 @@ import YClient from '../client';
 
 export default {
     name: "messageDeleteBulk",
-    execute: async (client: YClient, messages: Discord.Collection<Snowflake, Message>, channel: Discord.GuildTextBasedChannel) => {
+    execute: async (client: YClient, messages: Discord.Collection<string, Discord.Message<boolean>>, channel: Discord.GuildTextBasedChannel) => {
         if (!client.config.botSwitches.logs) return;
 
         const logChannel = client.channels.resolve(client.config.mainServer.channels.botlogs) as Discord.TextChannel;
-        let msgArray: Array<string> = [];
-
-        messages.forEach((e)=>{
-            msgArray.push(`${e.author.username}: ${e.content}`);
-        });
         const embed = new client.embed()
             .setTitle(`${messages.size} messages were deleted`)
-            .setDescription(`\`\`\`${msgArray.reverse().join('\n')}\`\`\``.slice(0, 3900))
+            .setDescription(`\`\`\`${messages.map((msg) => `${msg.author.username}: ${msg.content}`).reverse().join('\n').slice(0, 3900)}\`\`\``)
             .addFields({name: '🔹 Channel', value: `<#${channel.id}>`})
             .setColor(client.config.embedColor)
             .setTimestamp()
-        logChannel.send({embeds: [embed]})
-        
+        logChannel.send({embeds: [embed]});
     }
 }

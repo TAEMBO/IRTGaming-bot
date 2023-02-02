a4ceb3a0ffe31131c7df45f28a5caa0df7a2fe47
TÆMBØ <kuneshr123@gmail.com>
2023-02-01 17:13:33 -0800

    Combine API requests and logs

--- commands/loa.ts
+++ commands/loa.ts
@@ -5,13 +5,17 @@ export default {
         if (!client.isMPStaff(interaction.member)) return client.youNeedRole(interaction, "mpstaff");
         const roles = interaction.member.roles.cache.map((x, i) => i);
 
-        if (!interaction.member.roles.cache.has(client.config.mainServer.roles.loa)) {
-            await interaction.member.roles.set(roles.filter(x=>x != client.config.mainServer.roles.mpstaff).concat([client.config.mainServer.roles.loa]));
-            await interaction.member.setNickname(`[LOA] ${interaction.member.nickname}`).catch(() => console.log('failed to set nickname for LOA'));
+        if (!roles.includes(client.config.mainServer.roles.loa)) {
+            await interaction.member.edit({ 
+                roles: roles.filter(x=>x != client.config.mainServer.roles.mpstaff).concat([client.config.mainServer.roles.loa]),
+                nick: `[LOA] ${interaction.member.nickname}`
+            }).catch(() => interaction.member.roles.set(roles.filter(x=>x != client.config.mainServer.roles.mpstaff).concat([client.config.mainServer.roles.loa])));
             interaction.reply({content: 'LOA status set', ephemeral: true});
         } else {
-            await interaction.member.roles.set(roles.filter(x=>x != client.config.mainServer.roles.loa).concat([client.config.mainServer.roles.mpstaff]));
-            await interaction.member.setNickname(`${interaction.member.nickname?.replaceAll('[LOA] ', '')}`).catch(() => console.log('failed to set nickname for LOA'));
+            await interaction.member.edit({ 
+                roles: roles.filter(x=>x != client.config.mainServer.roles.loa).concat([client.config.mainServer.roles.mpstaff]),
+                nick: interaction.member.nickname?.replaceAll('[LOA] ', '')
+            }).catch(() => interaction.member.roles.set(roles.filter(x=>x != client.config.mainServer.roles.loa).concat([client.config.mainServer.roles.mpstaff])));
             interaction.reply({content: 'LOA status removed', ephemeral: true});
         }
 	},
--- events/guildMemberUpdate.ts
+++ events/guildMemberUpdate.ts
@@ -4,17 +4,22 @@ import YClient from '../client';
 export default async (client: YClient, oldMember: Discord.GuildMember, newMember: Discord.GuildMember) => {
     if (!client.config.botSwitches.logs) return;
 
+    let changes = false;
     const logChannel = client.channels.resolve(client.config.mainServer.channels.botlogs) as Discord.TextChannel;
+    const embed = new client.embed()
+        .setTimestamp()
+        .setColor(client.config.embedColor)
+        .setTitle(`Member Update: ${newMember.user.tag}`)
+        .setDescription(`<@${newMember.user.id}>\n\`${newMember.user.id}\``)
+        .setThumbnail(newMember.user.displayAvatarURL({ extension: 'png', size: 2048}));
 
     // Nickname changes
     if (oldMember.nickname != newMember.nickname) {
-        const embed = new client.embed().setColor(client.config.embedColor).setTitle(`Member Update: ${newMember.user.tag}`).setDescription(`<@${newMember.user.id}>\n\`${newMember.user.id}\``).setThumbnail(newMember.user.displayAvatarURL({ extension: 'png', size: 2048}))
-            .addFields(
+        changes = true;
+        embed.addFields(
                 {name: '🔹 Old Nickname', value: oldMember.nickname == null ? '*No nickname*' : `\`\`\`${oldMember.nickname}\`\`\``},
                 {name: '🔹 New Nickname', value: newMember.nickname == null ? '*No nickname*' : `\`\`\`${newMember.nickname}\`\`\``}
-            )
-            .setTimestamp()
-        logChannel.send({embeds: [embed]})
+        )
     }
 
     // Role changes
@@ -22,16 +27,16 @@ export default async (client: YClient, oldMember: Discord.GuildMember, newMember
     const oldRoles = oldMember.roles.cache.map((x, i) => i).filter(x => !newMember.roles.cache.map((x, i) => i).some(y => y == x));
 
     if ((newRoles.length != 0 || oldRoles.length != 0) && ((Date.now() - (newMember.joinedTimestamp as number)) > 5000)) {
-        const embed = new client.embed().setColor(client.config.embedColor).setTitle(`Member Update: ${newMember.user.tag}`).setDescription(`<@${newMember.user.id}>\n\`${newMember.user.id}\``).setThumbnail(newMember.user.displayAvatarURL({ extension: 'png', size: 2048})).setTimestamp()
-
         if (newRoles.length != 0) {
+            changes = true;
             embed.addFields({name: '🔹 Roles Added', value: newRoles.map((x) => `<@&${x}>`).join(' ')});
         }
         if (oldRoles.length != 0) {
+            changes = true;
             embed.addFields({name: '🔹 Roles Removed', value: oldRoles.map((x) => `<@&${x}>`).join(' ')});
         }
-        logChannel.send({embeds: [embed]});
     }
+    if (changes) logChannel.send({embeds: [embed]});
 
     // Trusted Farmer auto-updating list
     const mp_tf = (await (client.guilds.cache.get(client.config.mainServer.id) as Discord.Guild).roles.fetch(client.config.mainServer.roles.trustedfarmer) as Discord.Role).members;

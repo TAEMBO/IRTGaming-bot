53eef8f8db4ebc01a377a900d121dc3a3d4c73bf
TÆMBØ <kuneshr123@gmail.com>
2023-02-06 16:20:52 -0800

    Update mp.ts

--- commands/mp.ts
+++ commands/mp.ts
@@ -46,7 +46,9 @@ export default {
                 page.click(serverSelector).then(() => {
                     setTimeout(async () => {
                         await browser.close();
-                        interaction.editReply(result += `Total time taken: **${Date.now() - time}ms**`);
+                        if (chosenServer == 'ps') {
+                            interaction.editReply({content: result += `Total time taken: **${Date.now() - time}ms**`, files: ['../../Documents/My Games/FarmingSimulator2022/log.txt']});
+                        } else interaction.editReply(result += `Total time taken: **${Date.now() - time}ms**`);
                     }, 2000);
                 });
             });

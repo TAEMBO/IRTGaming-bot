4d14f75af772953931317730c015fcdda3a4beab
TÆMBØ <kuneshr123@gmail.com>
2023-02-10 21:14:58 -0800

    Move unPunish function into its commandFile

---
+++ commands/unpunish.ts
@@ -0,0 +1,27 @@
+import Discord, { SlashCommandBuilder } from 'discord.js';
+import YClient from '../client';
+import { db_punishments_format } from '../interfaces';
+export default {
+	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
+		if (!client.hasModPerms(interaction.member)) return client.youNeedRole(interaction, "mod");
+		
+		const punishment = (client.punishments._content as Array<db_punishments_format>).find(x => x.id === interaction.options.getInteger("case_id", true));
+		if (!punishment) return interaction.reply('That isn\'t a valid case ID');
+		if (punishment.expired) return interaction.reply('That case has already been overwritten');
+		if (['warn', 'mute'].includes(punishment.type) && interaction.member.roles.cache.has(client.config.mainServer.roles.helper)) return client.youNeedRole(interaction, "mod");
+		const reason = interaction.options.getString("reason") ?? 'Unspecified';
+		
+		await client.punishments.removePunishment(punishment.id, interaction.user.id, reason, interaction);
+	},
+	data: new SlashCommandBuilder()
+		.setName("unpunish")
+		.setDescription("Unpunish a member")
+		.addIntegerOption((opt) => opt
+			.setName("case_id")
+			.setDescription("The ID of the punishment to remove")
+			.setRequired(true))
+		.addStringOption((opt)=>opt
+			.setName("reason")
+			.setDescription("The reason for removing the punishment")
+			.setRequired(false))
+};

5ef0f455fe76e43d820ce4b9f03e0fc68d810795
TÆMBØ <kuneshr123@gmail.com>
2023-02-17 09:17:01 -0800

    Add userLevels requirement for applying

--- commands/apply.ts
+++ commands/apply.ts
@@ -1,15 +1,23 @@
 import Discord, { SlashCommandBuilder } from 'discord.js';
+import { db_userLevels_format } from 'interfaces';
 import YClient from '../client';
 export default {
 	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
         const applicationLogs = client.channels.resolve('811341461223112714') as Discord.TextChannel;
-        if ((Date.now() - (interaction.member.joinedTimestamp as number)) < 1209600000) { 
-            interaction.reply({content: 'You must be on the Discord server for at least two weeks before applying.', ephemeral: true});
-            applicationLogs.send(`<@${interaction.user.id}> (${interaction.user.tag}) tried to open an MP Staff application but was rejected due to insufficient time.`);
-        } else {
+        const eligibleTime = (Date.now() - (interaction.member.joinedTimestamp as number)) > 1209600000;
+        const eligibleMsgs = (client.userLevels._content[interaction.user.id] as db_userLevels_format).level > 3;
+        const deniedMsgs: Array<string> = [];
+
+        if (!eligibleTime) deniedMsgs.push('be on the Discord server for at least two weeks');
+        if (!eligibleMsgs) deniedMsgs.push('be more active on the Discord server');
+
+        if (deniedMsgs.length == 0) {
             interaction.reply({content: 'https://forms.gle/JivS14vJgcJCKigq7', ephemeral: true});
             applicationLogs.send(`<@${interaction.user.id}> (${interaction.user.tag}) opened an MP Staff application at <t:${Math.round(Date.now() / 1000)}>\nForm: <https://forms.gle/JivS14vJgcJCKigq7>`);
-        };
+        } else {
+            interaction.reply({content: `You need to ${deniedMsgs.join(' and ')} before applying`, ephemeral: true});
+            applicationLogs.send(`<@${interaction.user.id}> (${interaction.user.tag}) tried to open an MP Staff application but was rejected`);
+        }
     },
     data: new SlashCommandBuilder()
         .setName("apply")

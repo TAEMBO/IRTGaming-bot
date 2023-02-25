c4c557ee52d3a3bdfd8b146e3d93fbd33092ddde
TÆMBØ <kuneshr123@gmail.com>
2023-02-24 21:13:52 -0800

    Add modals

---
+++ commands/report.ts
@@ -0,0 +1,14 @@
+import Discord, { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
+import YClient from '../client';
+export default {
+	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
+		if (!client.isMPStaff(interaction.member)) return;
+        interaction.reply({components: [
+            new ActionRowBuilder<ButtonBuilder>().addComponents(
+                new ButtonBuilder().setStyle(ButtonStyle.Danger).setCustomId(`mpReport`).setLabel("MP Report test"))
+        ]});
+	},
+	data: new SlashCommandBuilder()
+		.setName("report")
+		.setDescription("TESTING TESTING 123 WAKEY WAKEY")
+};

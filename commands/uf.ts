00622a4138930b99220571a820f693c37b291fe0
TÆMBØ <kuneshr123@gmail.com>
2023-03-31 14:34:17 -0700

    Create uf.ts

---
+++ commands/uf.ts
@@ -0,0 +1,35 @@
+import Discord, { SlashCommandBuilder } from 'discord.js';
+import YClient from '../client.js';
+import FTPClient from 'ftp';
+import fs from 'node:fs';
+
+export default {
+	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
+        const whitelist = ['984568108704497694', '200066407415676928'];
+        if (!client.isMPStaff(interaction.member) && !whitelist.includes(interaction.member.id)) return client.youNeedRole(interaction, "mpstaff");
+        const chosenServer = interaction.options.getString('server', true) as 'ps' | 'pg';
+        const FTP = new FTPClient();
+
+        if (chosenServer == 'pg') {
+            await interaction.deferReply({ ephemeral: true });
+            FTP.connect(client.tokens.ftp.pg);
+            FTP.on('ready', () => FTP.get(client.tokens.ftp.pg.path + 'blockedUserIds.xml', async (err, stream) => {
+                if (err) return interaction.editReply(err.message);
+                stream.once('close', ()=>FTP.end());
+                stream.pipe(fs.createWriteStream('../databases/blockedUserIds.xml'));
+
+                setTimeout(() => interaction.editReply({files: ['../databases/blockedUserIds.xml']}), 1000);
+            }));
+        } else interaction.reply({files: ['../../../Documents/My Games/FarmingSimulator2022/blockedUserIds.xml'], ephemeral: true });
+	},
+	data: new SlashCommandBuilder()
+		.setName("uf")
+		.setDescription("Allows select members to download ban lists")
+        .addStringOption(x=>x
+            .setName('server')
+            .setDescription('The server to download bans from')
+            .addChoices(
+                { name: 'Public Silage', value: 'ps' },
+                { name: 'Public Grain', value: 'pg' })
+            .setRequired(true))
+};

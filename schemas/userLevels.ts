b244f7dcdd3e0a41da7c59e5c331035bab0a0ffe
TÆMBØ <kuneshr123@gmail.com>
2023-04-08 15:24:50 -0700

    Fix bug of inaccurate level displayment

--- schemas/userLevels.ts
+++ schemas/userLevels.ts
@@ -9,12 +9,11 @@ const Schema = mongoose.model('userLevels', new mongoose.Schema({
 }, { versionKey: false }));
 
 export default class userLevels extends Schema {
-    _content: typeof Schema;
-    constructor(public client: YClient) {
-		super();
-		this.client = client;
-		this._content = Schema;
-	}
+    public _content = Schema;
+    public algorithm = (level: number) => level * level * 15;
+    constructor(private client: YClient) {
+        super();
+    }
     async incrementUser(userid: string) {
         const userData = await this._content.findById(userid);
 
@@ -27,10 +26,9 @@ export default class userLevels extends Schema {
                 }
             } else if (userData.messages >= this.algorithm(userData.level+1)) {
                 userData.level++
-                (this.client.channels.resolve(this.client.config.mainServer.channels.botcommands) as Discord.TextChannel).send({content: `Well done <@${userid}>, you made it to **level ${userData.level + 1}**!`})
+                (this.client.channels.resolve(this.client.config.mainServer.channels.botcommands) as Discord.TextChannel).send({content: `Well done <@${userid}>, you made it to **level ${userData.level}**!`})
             }
             await userData.save();
         } else await this._content.create({ _id: userid, messages: 1, level: 0 });
     }
-    algorithm = (level: number) => level * level * 15;
 }

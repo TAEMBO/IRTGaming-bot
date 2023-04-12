60271eeb47f7dedb146a385c18d0bcf707cb240c
TÆMBØ <kuneshr123@gmail.com>
2023-04-11 20:22:56 -0700

    Revamp viewing of full watchList

--- commands/watch.ts
+++ commands/watch.ts
@@ -1,11 +1,14 @@
 import Discord, { SlashCommandBuilder } from 'discord.js';
 import YClient from '../client.js';
+import fs from 'node:fs';
+
 export default {
 	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
         if (!client.isMPStaff(interaction.member)) return client.youNeedRole(interaction, "mpstaff");
 
         const name = interaction.options.getString('username', false) as string;
         const wlData = await client.watchList._content.findById(name);
+
         ({
             add: async () => {
                 const reason = interaction.options.getString('reason', true);
@@ -21,8 +24,8 @@ export default {
                 } else interaction.reply(`\`${name}\` doesn't exist on watchList`);
             },
             view: async () => {
-                const list = await client.watchList._content.find();
-                interaction.reply(`\`\`\`json\n${JSON.stringify(list, null, 1)}\`\`\``)
+                fs.writeFileSync('../databases/watchListCache.json', JSON.stringify(await client.watchList._content.find(), null, 2));
+                interaction.reply({files: ['../databases/watchListCache.json']});
             }
         } as any)[interaction.options.getSubcommand()]();
     },

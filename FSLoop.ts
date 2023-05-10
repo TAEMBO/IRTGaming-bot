704070f272ca800074b236046fa00ec65b85e35e
TÆMBØ <kuneshr123@gmail.com>
2023-05-10 10:48:52 -0700

    Fix fault of message not being cached

--- FSLoop.ts
+++ FSLoop.ts
@@ -183,7 +183,7 @@ export function FSLoopAll(client: YClient) {
         if (server[1].players.length !== 0) embed.addFields({ name: `${server[0]} - ${serverSlots}/16`, value: playerInfo.join('\n') });
     }
 
-    (client.channels.cache.get(client.config.mainServer.channels.juniorAdminChat) as Discord.TextChannel).messages.cache.get(client.config.FSLoopMsgId)?.edit({
+    (client.channels.cache.get(client.config.mainServer.channels.juniorAdminChat) as Discord.TextChannel).messages.fetch(client.config.FSLoopMsgId).then(msg => msg.edit({
         embeds: [embed.setTitle(`All Servers: ${totalCount.reduce((a, b) => a + b, 0)} online`)]
-    });
+    })).catch(() => client.log('\x1b[31m', 'FSLoopAll invalid msg'));
 }

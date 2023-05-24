f22da80cca0a151dc08e2c0a1bb78c7872273fb6
TÆMBØ <kuneshr123@gmail.com>
2023-05-24 16:54:39 -0700

    Update unpunish.ts

--- commands/unpunish.ts
+++ commands/unpunish.ts
@@ -5,10 +5,10 @@ export default {
 	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
 		if (!client.hasModPerms(interaction.member)) return client.youNeedRole(interaction, 'discordmoderator');
 
-		const punishment = (await client.punishments._content.find()).find(x => x._id === interaction.options.getInteger("case_id", true));
-		if (!punishment) return interaction.reply('That isn\'t a valid case ID');
+		const punishment = await client.punishments._content.findById(interaction.options.getInteger('caseid', true));
+		if (!punishment) return interaction.reply('No case found with that ID');
 		if (punishment.expired) return interaction.reply('That case has already been overwritten');
-		if (['warn', 'mute'].includes(punishment.type) && interaction.member.roles.cache.has(client.config.mainServer.roles.discordhelper)) return client.youNeedRole(interaction, 'discordmoderator');
+		if (!['warn', 'mute'].includes(punishment.type) && interaction.member.roles.cache.has(client.config.mainServer.roles.discordhelper)) return client.youNeedRole(interaction, 'discordmoderator');
 		const reason = interaction.options.getString("reason") ?? 'Unspecified';
 		
 		await client.punishments.removePunishment(punishment.id, interaction.user.id, reason, interaction);
@@ -16,12 +16,12 @@ export default {
 	data: new SlashCommandBuilder()
 		.setName("unpunish")
 		.setDescription("Unpunish a member")
-		.addIntegerOption((opt) => opt
-			.setName("case_id")
-			.setDescription("The ID of the punishment to remove")
+		.addIntegerOption(x=>x
+			.setName("caseid")
+			.setDescription("The ID of the punishment to overwrite")
 			.setRequired(true))
-		.addStringOption((opt)=>opt
+		.addStringOption(x=>x
 			.setName("reason")
-			.setDescription("The reason for removing the punishment")
+			.setDescription("The reason for overwriting the punishment")
 			.setRequired(false))
 };

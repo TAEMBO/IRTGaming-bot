5a4bf21436244a8cc780440b8b26e88688d40d10
TÆMBØ <kuneshr123@gmail.com>
2023-05-27 14:44:58 -0700

    Update sharding.ts

--- sharding.ts
+++ sharding.ts
@@ -1,5 +1,5 @@
-import { token } from './config.json' assert { type: 'json' };
+import config from './config.json' assert { type: 'json' };
 import { ShardingManager } from 'discord.js';
-const sharder = new ShardingManager('./index.js', { token, totalShards: 1 });
+const sharder = new ShardingManager('./index.js', { token: config.token, totalShards: 1 });
 sharder.on("shardCreate", () => console.log('\x1b[32mShard started'));
 sharder.spawn();

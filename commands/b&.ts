96e244accfe5e601224d167a93f7c186b28bcaf2
TÆMBØ <kuneshr123@gmail.com>
2023-06-03 23:24:36 -0700

    Fix improper replies to interaction

--- commands/b&.ts
+++ commands/b&.ts
@@ -3,13 +3,13 @@ import YClient from '../client.js';
 export default {
 	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
         const user = interaction.options.getUser("member", true);
-        interaction.reply({ ephemeral: true }).then(() => interaction.deleteReply());
+        interaction.deferReply({ ephemeral: true }).then(() => interaction.deleteReply());
         interaction.channel?.send(`<@${user.id}> has received an honorary ban!`);
     },
     data: new SlashCommandBuilder()
         .setName("band")
         .setDescription("Introduce an honorary ban")
-        .addUserOption((opt)=>opt
+        .addUserOption(x=>x
             .setName("member")
             .setDescription("It's an honor")
             .setRequired(true))
--- commands/unb&.ts
+++ commands/unb&.ts
@@ -3,13 +3,13 @@ import YClient from '../client.js';
 export default {
 	async run(client: YClient, interaction: Discord.ChatInputCommandInteraction<"cached">) {
         const user = interaction.options.getUser("member", true);
-        interaction.reply({ ephemeral: true }).then(() => interaction.deleteReply());
+        interaction.deferReply({ ephemeral: true }).then(() => interaction.deleteReply());
         interaction.channel?.send(`<@${user.id}> had their honorary ban revoked!`);
     },
     data: new SlashCommandBuilder()
         .setName("unband")
-        .setDescription("Revokes an honorary ban")
-        .addUserOption((opt)=>opt
+        .setDescription("Revoke an honorary ban")
+        .addUserOption(x=>x
             .setName("member")
             .setDescription("It's an honor")
             .setRequired(true))

b927e891eabd04551796af3bb855d142af8bdac3
TÆMBØ <kuneshr123@gmail.com>
2023-06-17 18:54:44 -0700

    Update messageCreate.ts

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,34 +1,31 @@
 import Discord from 'discord.js';
 import YClient from '../client.js';
 
-export default async (client: YClient, message: Discord.Message) => {
+export default async (client: YClient, message: Discord.Message<boolean>) => {
     if ((!client.config.botSwitches.commands && !client.config.devWhitelist.includes(message.author.id)) || message.partial || message.author.bot) return;
-	//   ^^^     Bot is set to ignore commands and non-dev sent a message, ignore the message.      ^^^
+    //   ^^^     Bot is set to ignore commands and non-dev sent a message, ignore the message.      ^^^
 
-	const msg = message.content.replaceAll('\n', ' ').toLowerCase();
-	const msgarr = msg.split(' ');
+    const msg = message.content.replaceAll('\n', ' ').toLowerCase();
+    const msgarr = msg.split(' ');
 
-	if (!message.inGuild()) {
-		const guildMemberObject = client.mainGuild().members.cache.get(message.author.id) as Discord.GuildMember;
-    	client.getChan('taesTestingZone').send({
-			content: `DM Forward <@${client.config.devWhitelist[0]}>`,
-			files: message.attachments.map(x => x.url),
-			embeds: [new client.embed()
-				.setTitle('Forwarded DM Message')
-				.setDescription(`<@${message.author.id}>`)
-				.setAuthor({name: `${message.author.tag} (${message.author.id})`, iconURL: message.author.displayAvatarURL({ extension: 'png'})})
-				.setColor(client.config.embedColor)
-				.addFields({name: 'Message Content', value: message.content.length > 1024 ? message.content.slice(0, 1000) + '...' : message.content + '\u200b'})
-				.addFields({name: 'Roles:', value: guildMemberObject.roles.cache.size > 1 ? guildMemberObject.roles.cache.filter(x => x.id !== client.config.mainServer.id).sort((a, b) => b.position - a.position).map(x => x).join(guildMemberObject.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'})
-				.setTimestamp()]
-		});
+    if (!message.inGuild()) {
+        const guildMemberObject = client.mainGuild().members.cache.get(message.author.id) as Discord.GuildMember;
+        client.getChan('taesTestingZone').send({
+            content: `DM Forward <@${client.config.devWhitelist[0]}>`,
+            files: message.attachments.map(x => x.url),
+            embeds: [new client.embed()
+                .setTitle('Forwarded DM Message')
+                .setDescription(`<@${message.author.id}>`)
+                .setAuthor({ name: `${message.author.tag} (${message.author.id})`, iconURL: message.author.displayAvatarURL({ extension: 'png' }) })
+                .setColor(client.config.embedColor)
+                .setTimestamp()
+                .setFields(
+                    { name: 'Message Content', value: message.content.length > 1024 ? message.content.slice(0, 1000) + '...' : message.content + '\u200b' },
+                    { name: 'Roles:', value: guildMemberObject.roles.cache.size > 1 ? guildMemberObject.roles.cache.filter(x => x.id !== client.config.mainServer.id).sort((a, b) => b.position - a.position).map(x => x).join(guildMemberObject.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None' })
+            ]
+        });
     } else {
 		let automodded = false;
-
-		/* judge-your-build-event message filter; only allow messages that contain an image
-		if (message.channel.id === '925500847390097461' && message.attachments.size<1 && !message.author.bot) {
-			  message.delete();
-		} */
 	
 		// useless staff ping mute
 		if (message.mentions.roles.some(mentionedRole => mentionedRole.id === client.config.mainServer.roles.mpstaff)) {
@@ -36,27 +33,26 @@ export default async (client: YClient, message: Discord.Message) => {
 			message.channel.awaitMessages({
 				filter: x => client.isMPStaff(x.member as Discord.GuildMember) && x.content === 'y',
 				max: 1,
-				time: 60000,
-				errors: ["time"]
+				time: 60000
 			}).then(async collected => {
 				const colMsg = collected.first() as Discord.Message<true>;
 				client.log('\x1b[35m', `Received "y" from ${colMsg.author.tag}, indicating to mute`);
 				await client.punishments.addPunishment('mute', colMsg.author.id, 'Automod; Misuse of staff ping', message.author, message.member, { time: '10m' });
 				colMsg.react('✅');
-			}).catch(() => client.log('\x1b[35m', 'Failed to collect "y" from staff'));
+			});
 		}
 	
 		const Whitelist = [
-			'688803177184886794', //farm-manager-chat
-			'906960370919436338', //mp-action-log
-			'830916009107652630', //public-admin-chat
-			'733828561215029268', //mp-staff-commands
-			'690549465559597127', //mp-ban-list
-			'677146047868436480', //mp-admin-chat
-			'828982825734504448', //mf-manager-chat
-			'986969325112033330', //mf-serverlog
-			'968265015595532348', //mp-manager-chat
-			'979863373439184966', //war crimes
+			'688803177184886794', // farm-manager-chat
+			'906960370919436338', // mp-action-log
+			'830916009107652630', // junior-admin-chat
+			'733828561215029268', // mp-staff-commands
+			'690549465559597127', // mp-ban-list
+			'677146047868436480', // senior-admin-chat
+			'828982825734504448', // mf-manager-chat
+			'986969325112033330', // mf-serverlog
+			'968265015595532348', // mp-manager-chat
+			'979863373439184966', // war crimes
 		];
 		async function repeatedMessages(thresholdTime: number, thresholdAmt: number, type: string, muteTime?: string, muteReason?: string) {
 			if (client.repeatedMessages[message.author.id]) {
@@ -77,50 +73,44 @@ export default async (client: YClient, message: Discord.Message) => {
 	
 				if (spammedMessage) {
 					delete client.repeatedMessages[message.author.id];
-					await client.punishments.addPunishment('mute', (client.user as Discord.User).id, `Automod; ${muteReason}`, message.author, message.member, { time: muteTime });
+					await client.punishments.addPunishment('mute', (client.user?.id as string), `Automod; ${muteReason}`, message.author, message.member, { time: muteTime });
 				}
 			} else {
 				client.repeatedMessages[message.author.id] = { data: new client.collection(), timeout: setTimeout(() => delete client.repeatedMessages[message.author.id], thresholdTime) };
 				client.repeatedMessages[message.author.id].data.set(message.createdTimestamp, { type, channel: message.channel.id });
 			}
 		}
-		function combos(word: string) {
-			let val = [];
-			let chop = word[0];
-			for (let i = 1; i <= word.length; i++) {
-				if (chop[0] == word[i]) {
-					chop += word[i];
-				} else {
-					val.push(chop);
-					chop = word[i];
-				}
-			}
-			let arr = [];
-			for (let i = 0; i < val.length; i++) {
-				let temp = [];
-				if (val[i].length >= 2) temp.push(val[i][0].repeat(2));
-				temp.push(val[i][0]);
-				arr.push(temp);
-			}
-			return arr;
-		}
 		let loop = 0;
-		function allPossibleCases(arr: Array<Array<string>>): Array<string> {
+		function allPossibleCases(arr: string[][]): string[] {
 			loop++;
-			if (arr.length === 1 || loop > 30) return arr[0];
-			var result = [];
-			var allCasesOfRest = allPossibleCases(arr.slice(1));
-			for (var i = 0; i < allCasesOfRest.length; i++) {
-				for (var j = 0; j < arr[0].length; j++) {
-					result.push(arr[0][j] + allCasesOfRest[i]);
-				};
+			if (arr.length === 1 || loop > 20) return arr[0];
+			const result = [];
+			const allCasesOfRest = allPossibleCases(arr.slice(1));
+			for (let i = 0; i < allCasesOfRest.length; i++) {
+				for (let j = 0; j < arr[0].length; j++) result.push(arr[0][j] + allCasesOfRest[i]);
 			}
 			return result;
 		}
-		const getAllCombos = (message: string) => message.split(" ").map(w => {
-			if (/(.)\1{1,}/.test(w) && w.length > 3) {
-				return allPossibleCases(combos(w)).join(' ');
-			} else return w;
+		const getAllCombos = (message: string) => message.split(" ").map(word => {
+			if (/(.)\1{1,}/.test(word) && word.length > 3) {
+                const val = [];
+                const arr = [];
+                let chop = word[0];
+                for (let i = 1; i <= word.length; i++) {
+                    if (chop[0] != word[i]) {
+                        val.push(chop);
+                        chop = word[i];
+                    } else chop += word[i];
+    
+                }
+                for (let i = 0; i < val.length; i++) {
+                    let temp = [];
+                    if (val[i].length >= 2) temp.push(val[i][0].repeat(2));
+                    temp.push(val[i][0]);
+                    arr.push(temp);
+                }
+				return allPossibleCases(arr).join(' ');
+			} else return word;
 		});
 		let newMsg = msg;
 		new Map([[/!/g, "i"], [/@/g, "a"], [/\$/g, "s"], [/3/g, "e"], [/1/g, "i"], [/¡/g, "i"],[/5/g, "s"], [/0/g, "o"], [/4/g, "h"], [/7/g, "t"], [/9/g, "g"], [/6/g, "b"], [/8/g, "b"]]).forEach((str, reg) => newMsg = newMsg.replace(reg, str));
@@ -137,7 +127,7 @@ export default async (client: YClient, message: Discord.Message) => {
 				await repeatedMessages(30000, 4, 'bw', '30m', 'Banned words');
 			} else if (msg.includes("discord.gg/") && !client.isMPStaff(message.member as Discord.GuildMember)) { // Discord advertisement
 				const inviteURL = message.content.split(' ').find(x => x.includes('discord.gg/')) as string;
-				const validInvite = await client.fetchInvite(inviteURL).catch(() => undefined);
+				const validInvite = await client.fetchInvite(inviteURL).catch(() => null);
 				if (validInvite && validInvite.guild?.id !== client.config.mainServer.id) {
 					automodded = true;
 					await message.reply("No advertising other Discord servers.").then(msg => {
@@ -149,15 +139,16 @@ export default async (client: YClient, message: Discord.Message) => {
 			}
 		}
 
-		if (automodded) return;
-		if (message.channel.id !== '557692151689904129') client.userLevels.incrementUser(message.author.id);
-		if (!client.config.botSwitches.autoResponses) return;
+        if (automodded) return;
+        if (message.channel.id !== '557692151689904129') client.userLevels.incrementUser(message.author.id);
+        if (!client.config.botSwitches.autoResponses || !message.member) return;
 			
-		// Morning message system
-		const person = message.member?.displayName;
-		const morningMsgs = [ 'morning all', 'morning everyone', 'morning guys', 'morning people' ];
-		const mornRes1 = [ `Wakey wakey ${person}! `, `Morning ${person}! `, `Why good morning ${person}! `, `Rise and shine ${person}! `, `Up and at 'em ${person}! `];
-		const mornRes2 = [
+        // Morning message system
+        const hasStaffTag = message.member.displayName.indexOf(' | ') < 0 ? false : true;
+        const person = message.member.displayName.slice(0, hasStaffTag ? message.member.displayName.indexOf(' | ') : undefined);
+        const morningMsgs = [ 'morning all', 'morning everyone', 'morning guys', 'morning people' ];
+        const mornRes1 = [ `Wakey wakey ${person}! `, `Morning ${person}! `, `Why good morning ${person}! `, `Rise and shine ${person}! `, `Up and at 'em ${person}! `];
+        const mornRes2 = [
 			'Here, take a pancake or two 🥞',
 			'Here, take a 🥔',
 			'Here, take a cookie 🍪',
@@ -180,7 +171,12 @@ export default async (client: YClient, message: Discord.Message) => {
 		];
 	
 		// Auto responses
-		if (morningMsgs.some(x => msg.includes(x)) && message.channel.id === client.config.mainServer.channels.general) message.reply({ content: mornRes1[Math.floor(Math.random() * mornRes1.length)] + mornRes2[Math.floor(Math.random() * mornRes2.length)], allowedMentions: { repliedUser: false } });
+        const randomEl = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
+        
+		if (morningMsgs.some(x => msg.includes(x)) && message.channel.id === client.config.mainServer.channels.general) message.reply({
+            content: randomEl(mornRes1) + randomEl(mornRes2),
+            allowedMentions: { repliedUser: false }
+        });
 		
 		if (msg.includes('giants moment')) message.react('™️');
 		
@@ -192,6 +188,6 @@ export default async (client: YClient, message: Discord.Message) => {
 		
 		if (msgarr.includes('69')) message.react(':IRT_Noice:611558357643558974');
 		
-		if (msg.startsWith('!rank')) message.reply({content: 'Ranking has been moved to </rank view:1042659197919178790>', allowedMentions: {repliedUser: false}});
+		if (msg.startsWith('!rank')) message.reply({ content: 'Ranking has been moved to </rank view:1042659197919178790>', allowedMentions: { repliedUser: false } });
 	}
 }

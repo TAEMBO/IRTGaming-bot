f15ab63593af1a9596e35e159c0563f0489f54c5
TÆMBØ <kuneshr123@gmail.com>
2023-07-12 12:14:26 -0700

    Fix bug with exceeded length

--- utilities/errorLog.ts
+++ utilities/errorLog.ts
@@ -6,7 +6,6 @@ import { getChan } from '../utilities.js';
  * @param client 
  * @param error The error itself
  * @param event The name of the event that this error originated from
- * @returns 
  */
 export function errorLog(client: YClient, error: Error, event: string) {
     if (['Request aborted', 'getaddrinfo ENOTFOUND discord.com'].includes(error.message) || !client.isReady()) return;
@@ -16,7 +15,7 @@ export function errorLog(client: YClient, error: Error, event: string) {
     getChan(client, 'taesTestingZone').send({
         content: `<@${client.config.devWhitelist[0]}>`,
         embeds: [new client.embed()
-            .setTitle(`Error Caught - ${error.message}`)
+            .setTitle(`Error Caught - ${error.message.slice(0, 240)}`)
             .setColor("#420420")
             .setDescription(`\`\`\`ansi\n${error.stack?.replaceAll(' at ', ' [31mat[37m ').replaceAll(dirname, `[33m${dirname}[37m`).slice(0, 2500)}\`\`\``)
             .setTimestamp()

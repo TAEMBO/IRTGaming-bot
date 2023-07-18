31aa8facbb8103433a696b28520dc4ce7cd4efb1
TÆMBØ <kuneshr123@gmail.com>
2023-07-17 23:22:06 -0700

    Update inviteCreate.ts

--- events/inviteCreate.ts
+++ events/inviteCreate.ts
@@ -1,4 +1,4 @@
 import Discord from 'discord.js';
 import YClient from '../client.js';
 
-export default async (client: YClient, invite: Discord.Invite) => (await (invite.guild as Discord.Guild).invites.fetch()).forEach(inv => client.invites.set(inv.code, {uses: inv.uses, creator: inv.inviter?.id}));
+export default async (client: YClient, invite: Discord.Invite) => client.invites.set(invite.code, { uses: invite.uses, creator: invite.inviter?.id });

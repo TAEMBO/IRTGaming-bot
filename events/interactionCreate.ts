2011a101c30be8e1a60b158f87574ca08ade04ff
TÆMBØ <kuneshr123@gmail.com>
2023-07-17 23:01:32 -0700

    Update interactionCreate.ts

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -10,13 +10,13 @@ export default async (client: YClient, interaction: Discord.Interaction<Discord.
         if (interaction.commandName === 'ping') return interaction.reply({ content: 'Pinging...', fetchReply: true }).then(msg => msg.edit(`Websocket: \`${client.ws.ping}\`ms\nBot: \`${msg.createdTimestamp - interaction.createdTimestamp}\`ms`));
         
         const subCmd = interaction.options.getSubcommand(false);
-        const commandFile = client.commands.get(interaction.commandName) as Command;
+        const command = client.commands.get(interaction.commandName) as Command;
 
         log(LogColor.White, `\x1b[32m${interaction.user.tag}\x1b[37m used \x1b[32m/${interaction.commandName} ${subCmd ?? ''}\x1b[37m in \x1b[32m#${interaction.channel?.name}`);
         if (!client.config.botSwitches.commands && !client.config.devWhitelist.includes(interaction.user.id)) return interaction.reply('Commands are currently disabled.');
 
-        commandFile.commandFile.default.run(client, interaction);
-        commandFile.uses ? commandFile.uses++ : commandFile.uses = 1;
+        command.commandFile.default.run(client, interaction);
+        command.uses++
     } else if (interaction.isButton()) {
         if (interaction.customId.startsWith('reaction-') && client.config.botSwitches.buttonRoles) { // Button roles
             const RoleID = interaction.customId.replace('reaction-', '');

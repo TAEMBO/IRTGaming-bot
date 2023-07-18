bd71601562f21a6452661cbf867ae4c96ab6bf4c
TÆMBØ <kuneshr123@gmail.com>
2023-07-17 23:01:29 -0700

    Update whois.ts

--- commands/whois.ts
+++ commands/whois.ts
@@ -1,4 +1,4 @@
-import Discord, { APIEmbedField, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import Discord, { APIEmbedField, ActivityType, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import YClient from '../client.js';
 import { ApplicationRPC } from '../typings.js';
 
@@ -29,125 +29,133 @@ export default {
                 invisible: "⚫"
             }[status ?? 'invisible'];
         }
+        
+        const member = interaction.options.getMember("member");
 
-		const member = interaction.options.getMember("member");
-
-		if (!member) {
-			const user = interaction.options.getUser('member', true);
+        if (!member) {
+            const user = interaction.options.getUser('member', true);
             const appData = await getApplicationData(user.id);
             const embed = new client.embed()
                 .setThumbnail(user.displayAvatarURL({ extension: 'png', size: 2048 }))
                 .setTitle(`${user.bot ? 'Bot' : 'User'} info: ${user.tag}`)
                 .setURL(`https://discord.com/users/${user.id}`)
                 .setDescription(`<@${user.id}>\n\`${user.id}\``)
-                .addFields({ name: '🔹 Account Created', value: `<t:${Math.round(user.createdTimestamp / 1000)}:R>` })
+                .addFields({ name: `🔹 ${user.bot ? 'Bot' : 'Account'} Created`, value: `<t:${Math.round(user.createdTimestamp / 1000)}:R>` })
                 .setColor(client.config.embedColor);
 
             if (appData) embed.addFields(...appData);
 
-			interaction.reply({ embeds: [embed] });
-		} else {
-			await member.user.fetch();
-			const embeds: EmbedBuilder[] = [];
-			let titleText = 'Member';
+            return interaction.reply({ embeds: [embed] });
+        }
 
-			if (member.user.bot) {
-				titleText = 'Bot';
-			} else if (member.user.id === interaction.guild.ownerId) titleText = ':crown: Server Owner';
-			
-			embeds.push(new client.embed()
-				.setThumbnail(member.user.displayAvatarURL({ extension: 'png', size: 2048 }))
-				.setTitle(`${titleText} info: ${member.user.tag}`)
-				.setURL(`https://discord.com/users/${member.user.id}`)
-				.setDescription(`<@${member.user.id}>\n\`${member.user.id}\``)
-				.addFields(
-					{ name: '🔹 Account created', value: `<t:${Math.round(member.user.createdTimestamp / 1000)}:R>` },
-					{ name: '🔹 Joined Server', value: `<t:${Math.round(member.joinedTimestamp as number / 1000)}:R>` },
-					{ name: `🔹 Roles: ${member.roles.cache.size - 1}`, value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guildId).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None' })
-				.setColor(member.displayColor || '#ffffff')
-				.setImage(member.user.bannerURL({ extension: 'png', size: 1024 }) ?? null)
-            );
+        await member.user.fetch();
+        const embeds: EmbedBuilder[] = [];
+        let titleText = 'Member';
+        
+        if (member.user.bot) {
+            titleText = 'Bot';
+        } else if (member.user.id === interaction.guild.ownerId) titleText = ':crown: Server Owner';
+        
+        embeds.push(new client.embed()
+            .setThumbnail(member.user.displayAvatarURL({ extension: 'png', size: 2048 }))
+            .setTitle(`${titleText} info: ${member.user.tag}`)
+            .setURL(`https://discord.com/users/${member.user.id}`)
+            .setDescription(`<@${member.user.id}>\n\`${member.user.id}\``)
+            .addFields(
+                { name: '🔹 Account created', value: `<t:${Math.round(member.user.createdTimestamp / 1000)}:R>` },
+                { name: '🔹 Joined server', value: `<t:${Math.round(member.joinedTimestamp as number / 1000)}:R>` },
+                { name: `🔹 Roles: ${member.roles.cache.size - 1}`, value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guildId).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None' })
+            .setColor(member.displayColor || '#ffffff')
+            .setImage(member.user.bannerURL({ extension: 'png', size: 1024 }) ?? null)
+        );
 
-			if (member.premiumSinceTimestamp) embeds[0].addFields({ name: '🔹 Server Boosting Since', value: `<t:${Math.round(member.premiumSinceTimestamp / 1000)}:R>`, inline: true });
-			
-            if (member.user.bot) {
-                const appData = await getApplicationData(member.user.id);
+        if (member.premiumSinceTimestamp) embeds[0].addFields({ name: '🔹 Server Boosting Since', value: `<t:${Math.round(member.premiumSinceTimestamp / 1000)}:R>`, inline: true });
+        	
+        if (member.user.bot) {
+            const appData = await getApplicationData(member.user.id);
 
-                if (appData) embeds[0].addFields(...appData);
+            if (appData) embeds[0].addFields(...appData);
 
-                return interaction.reply({ embeds });
+            return interaction.reply({ embeds });
+        }
+
+        if (!member.presence) return interaction.reply({ embeds });
+
+        embeds[0].addFields({
+            name: `🔹 Status: ${member.presence.status}`,
+            value: `${member.presence.status === 'offline' ? '\u200b' : [
+                `Web: ${convertStatus(member.presence.clientStatus?.web)}`,
+                `Mobile: ${convertStatus(member.presence.clientStatus?.mobile)}`,
+                `Desktop: ${convertStatus(member.presence.clientStatus?.desktop)}`
+            ].join('\n')}`,
+            inline: true
+        });
+
+        for (const activity of member.presence.activities) {
+            if (activity.type === ActivityType.Listening && activity.details && activity.assets) {
+                embeds.push(new client.embed()
+                    .setAuthor({ name: activity.name, iconURL: 'https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png' })
+                    .setColor('#1DB954')
+                    .setFields({ name: activity.details, value: `By: ${activity.state}\nOn: ${activity.assets.largeText}\nStarted listening <t:${Math.round(activity.createdTimestamp / 1000)}:R>` })
+                    .setThumbnail(`https://i.scdn.co/image/${activity.assets.largeImage?.replace('spotify:', '')}`)
+                );
+            } else if (activity.type === ActivityType.Custom) {
+                embeds.push(new client.embed()
+                .setTitle(activity.name)
+                    .setColor('#ffffff')
+                    .setDescription([
+                        activity.emoji ? `**Emoji name:** ${activity.emoji.name}`: '',
+                        activity.state ? `\n**Text:** ${activity.state}`: ''
+                    ].join(''))
+                );
+            } else {
+                function formatImgURL(type: 'assets' | 'icons', path: string | null | undefined) {
+                    return `https://cdn.discordapp.com/app-${type}/${activity.applicationId}/${path}.png`;
+                }
+
+                const activityImages: Record<string, string> = {
+                    '1129504162200166401': formatImgURL('icons', '469676e3ad37898c0289283c30c2c882'), // Farming Simulator 22
+                    '542474758835535872': formatImgURL('icons', '37b18c2d5633628d936dd3b2b083785b'), // Farming Simulator 19
+                    '363426921612181504': formatImgURL('icons', '61bed87d2da8e32dd8f24423a9e83323'), // Farming Simulator 17
+                    '451556128992657418': formatImgURL('icons', '48cfba535d49560a086fe55de2e2743b'), // Farming Simulator 15
+                    '732565262704050298': formatImgURL('assets', activity.assets?.largeImage), // Visual Studio Code
+                    '383226320970055681': formatImgURL('assets', activity.assets?.largeImage), // Visual Studio Code
+                    '356875570916753438': formatImgURL('icons', '166fbad351ecdd02d11a3b464748f66b'), // Minecraft
+                    '438122941302046720': 'https://discord.com/assets/29b4af8bf13fa73258692008d25b4f0d.png', // Xbox
+                    '356876176465199104': formatImgURL('icons', '069d9f4871b5ebd2f62bd342ce6ba77f'), // Grand Theft Auto V
+                    '363445589247131668': formatImgURL('icons', 'f2b60e350a2097289b3b0b877495e55f'), // Roblox
+                    '356876590342340608': formatImgURL('icons', '554af7ef210877b5f04fd1b727a3746e'), // Rainbow Six Siege
+                    '445956193924546560': formatImgURL('assets', activity.assets?.largeImage), // Rainbow Six Siege
+                    '432980957394370572': formatImgURL('assets', activity.assets?.largeImage) // Fortnite
+                };
+
+                let activityImage: string | null | undefined = activityImages[activity.applicationId ?? ''] as string | undefined;
+                
+                if (!activityImage) activityImage = activity.assets?.largeImageURL(); // PlayStation
+                if (!activityImage) activityImage = activity.assets?.smallImageURL(); // Residual images
+
+                embeds.push(new client.embed()
+                    .setTitle(activity.name)
+                    .setColor('#ffffff')
+                    .setDescription([
+                        `\u200b**Started:** <t:${Math.round(activity.createdTimestamp / 1000)}:R>`,
+                        activity.details ? '\n**Details:** ' + activity.details : '',
+                        activity.state ? '\n**State:** ' + activity.state : '',
+                        activity.assets?.largeText ? '\n**Large text:** ' + activity.assets.largeText : '',
+                        activity.assets?.smallText ? '\n**Small text:** ' + activity.assets.smallText : ''
+                    ].join(''))
+                    .setThumbnail(activityImage ?? null)
+                );
             }
-
-            if (!member.presence) return interaction.reply({ embeds });
-
-            embeds[0].addFields({
-                name: `🔹 Status: ${member.presence.status}`,
-                value: `${member.presence.status === 'offline' ? '\u200b' : [
-                    `Web: ${convertStatus(member.presence.clientStatus?.web)}`,
-                    `Mobile: ${convertStatus(member.presence.clientStatus?.mobile)}`,
-                    `Desktop: ${convertStatus(member.presence.clientStatus?.desktop)}`
-                ].join('\n')}`,
-                inline: true
-            });
-
-            for (const activity of member.presence.activities) {
-                if (activity.type === 2 && activity.details && activity.assets) {
-					embeds.push(new client.embed()
-						.setAuthor({ name: activity.name, iconURL: 'https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png' })
-						.setColor('#1DB954')
-						.setFields({ name: activity.details, value: `By: ${activity.state}\nOn: ${activity.assets.largeText}\nStarted listening <t:${Math.round(activity.createdTimestamp / 1000)}:R>` })
-						.setThumbnail(`https://i.scdn.co/image/${activity.assets.largeImage?.replace('spotify:', '')}`)
-                    );
-				} else if (activity.type === 4) {
-					embeds.push(new client.embed()
-						.setTitle(activity.name)
-						.setColor('#ffffff')
-						.setDescription([
-							activity.emoji ? `**Emoji name:** ${activity.emoji.name}`: '',
-							activity.state ? `\n**Text:** ${activity.state}`: ''
-						].join(''))
-                    );
-				} else {
-					let activityImage: string[] | string | null | undefined = [
-						['542474758835535872', 'https://cdn.discordapp.com/app-icons/542474758835535872/37b18c2d5633628d936dd3b2b083785b.png'], // Farming Simulator 19
-						['363426921612181504', 'https://cdn.discordapp.com/app-icons/363426921612181504/61bed87d2da8e32dd8f24423a9e83323.png'], // Farming Simulator 17
-						['732565262704050298', `https://cdn.discordapp.com/app-assets/732565262704050298/${activity.assets?.largeImage}.png`], // Visual Studio Code
-						['383226320970055681', `https://cdn.discordapp.com/app-assets/383226320970055681/${activity.assets?.largeImage}.png`], // Visual Studio Code
-						['356875570916753438', 'https://cdn.discordapp.com/app-icons/356875570916753438/166fbad351ecdd02d11a3b464748f66b.png'], // Minecraft
-						['438122941302046720', 'https://discord.com/assets/29b4af8bf13fa73258692008d25b4f0d.png'], // Any Xbox status
-						['356876176465199104', 'https://cdn.discordapp.com/app-icons/356876176465199104/069d9f4871b5ebd2f62bd342ce6ba77f.png'], // Grand Theft Auto V
-						['363445589247131668', 'https://cdn.discordapp.com/app-icons/363445589247131668/f2b60e350a2097289b3b0b877495e55f.png'], // Roblox
-						['356876590342340608', 'https://cdn.discordapp.com/app-icons/356876590342340608/554af7ef210877b5f04fd1b727a3746e.png'], // Rainbow Six Siege
-						['445956193924546560', `https://cdn.discordapp.com/app-assets/445956193924546560/${activity.assets?.largeImage}.png`], // Rainbow Six Siege again
-					].find(x => x[0] === activity.applicationId);
-
-					if (activityImage) activityImage = activityImage[1]; // If a URL was found in the array, choose the URL string in the array
-					if (!activityImage) activityImage = activity.assets?.largeImageURL(); // Struggle with PlayStation presence images
-					if (!activityImage) activityImage = activity.assets?.smallImageURL(); // Anything extra
-
-					embeds.push(new client.embed()
-						.setTitle(activity.name)
-						.setColor('#ffffff')
-						.setDescription([
-							`\u200b**Started:** <t:${Math.round(activity.createdTimestamp/1000)}:R>`,
-							activity.details ? '\n**Details:** ' + activity.details : '',
-							activity.state ? '\n**State:** ' + activity.state : '',
-							activity.assets?.largeText ? '\n**Large text:** ' + activity.assets.largeText : '',
-							activity.assets?.smallText ? '\n**Small text:** ' + activity.assets.smallText : ''
-						].join(''))
-						.setThumbnail(activityImage ?? null)
-					);
-				}
-            }
-            
-			interaction.reply({ embeds });
-		}
-	},
-	data: new SlashCommandBuilder()
-		.setName("whois")
-		.setDescription("Get info on a member or user")
-		.addUserOption(x=>x
-			.setName("member")
-			.setDescription("The member or user to get info on")
-			.setRequired(true))
+        }
+        
+        interaction.reply({ embeds });
+    },
+    data: new SlashCommandBuilder()
+        .setName("whois")
+        .setDescription("Get info on a member or user")
+        .addUserOption(x=>x
+            .setName("member")
+            .setDescription("The member or user to get info on")
+            .setRequired(true))
 };

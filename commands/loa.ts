7f06d8f9c7c0465b2a385a3bc3481fe9b292706f
TÆMBØ <kuneshr123@gmail.com>
2023-08-03 15:56:17 -0700

    Update loa.ts

--- commands/loa.ts
+++ commands/loa.ts
@@ -1,31 +1,39 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { TInteraction } from '../typings.js';
-import { isMPStaff, youNeedRole } from '../utilities.js';
+import { TInteraction } from '../typings.js'; 
+import { hasRole, isMPStaff, youNeedRole } from '../utilities.js';
 
 export default {
 	async run(interaction: TInteraction) {
         if (!isMPStaff(interaction)) return youNeedRole(interaction, "mpstaff");
 
         const roles = interaction.member.roles.cache.map((x, i) => i);
-        const allRoles = interaction.client.config.mainServer.roles;
+        const configRoles = interaction.client.config.mainServer.roles;
 
-        if (!roles.includes(allRoles.loa)) {
-            await interaction.member.edit({ 
-                roles: roles.filter(x=>x != allRoles.mpstaff).concat([allRoles.loa]),
+        if (!roles.includes(configRoles.loa)) {
+            const takenRoles = roles.filter(x => x != configRoles.mpstaff && x != configRoles.mpmanagement).concat([configRoles.loa]);
+
+            await interaction.member.edit({
+                roles: takenRoles,
                 nick: `[LOA] ${interaction.member.nickname}`
-            }).catch(() => interaction.member.roles.set(roles.filter(x=>x != allRoles.mpstaff).concat([allRoles.loa])));
+            }).catch(() => interaction.member.roles.set(takenRoles));
 
             interaction.reply({ content: 'LOA status set', ephemeral: true });
         } else {
-            await interaction.member.edit({ 
-                roles: roles.filter(x=>x != allRoles.loa).concat([allRoles.mpstaff]),
+            const returnedRoles = (() => {
+                if (hasRole(interaction, 'mpmanager')) {
+                    return roles.filter(x => x !== configRoles.loa).concat([configRoles.mpstaff, configRoles.mpmanagement]);
+                } else return roles.filter(x => x !== configRoles.loa).concat([configRoles.mpstaff]);
+            })();
+            
+            await interaction.member.edit({
+                roles: returnedRoles,
                 nick: interaction.member.nickname?.replaceAll('[LOA] ', '')
-            }).catch(() => interaction.member.roles.set(roles.filter(x=>x != allRoles.loa).concat([allRoles.mpstaff])));
+            }).catch(() => interaction.member.roles.set(returnedRoles));
 
             interaction.reply({ content: 'LOA status removed', ephemeral: true });
         }
 	},
 	data: new SlashCommandBuilder()
         .setName("loa")
-        .setDescription("Set yourself as LOA")
+        .setDescription("Manage your LOA status")
 };

c3d2636ab10d25cf8d2ac007c0c39e1357f0dc1a
TÆMBØ <kuneshr123@gmail.com>
2023-08-06 10:23:28 -0700

    Update appeal.ts

--- commands/appeal.ts
+++ commands/appeal.ts
@@ -1,21 +1,23 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 import { TInteraction } from '../typings.js';
-import { isMPStaff } from '../utilities.js';
+import { FSServers, isMPStaff } from '../utilities.js';
 
 export default {
 	async run(interaction: TInteraction) {
         if (interaction.channel?.parentId !== '980947706736427019' && !isMPStaff(interaction)) return interaction.reply({ content: 'You cannot use this command here', ephemeral: true });
 
+        const fsServers = new FSServers(interaction.client.config.fs);
+
         interaction.reply({ embeds: [new EmbedBuilder()
             .setTitle('MP Support - Ban Appeal')
             .setColor(interaction.client.config.embedColor)
             .setDescription([
-                'To appeal a ban on one of our FS servers, please provide the following information:',
-                '<:IRTDot:908818924286648350> In-game name at the time of being banned',
-                '<:IRTDot:908818924286648350> Rough time and date',
-                '<:IRTDot:908818924286648350> Server name (Public Silage or Public Grain)',
-                '<:IRTDot:908818924286648350> What you were doing at the time of being banned',
-                '<:IRTDot:908818924286648350> Anything else which could help with the appeal',
+                'To appeal a ban on one of our MP servers, please provide the following information:',
+                '- In-game name at the time of being banned',
+                '- Rough time and date',
+                `- Server name (${fsServers.getPublicNames().join(' or ')})`,
+                '- If applicable; what you were doing at the time of being banned',
+                '- Anything else which could help with the appeal, e.g. additional names you\'ve used',
                 '\u200b',
                 'Once you have done so, we will review your ban as soon as possible. Please be patient as we may have to wait for staff in other time zones before making a decision.'
             ].join('\n'))

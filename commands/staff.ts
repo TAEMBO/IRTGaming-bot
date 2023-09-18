e62909ba445fdf86f64227a27e3b8b6c83065a89
TÆMBØ <kuneshr123@gmail.com>
2023-09-17 23:53:36 -0700

    Update staff.ts

--- commands/staff.ts
+++ commands/staff.ts
@@ -1,10 +1,8 @@
 import { SlashCommandBuilder, EmbedBuilder, GuildMember } from 'discord.js';
-import { TInteraction } from '../typings.js';
+import { Index, TInteraction } from '../typings.js';
 
 export default {
 	async run(interaction: TInteraction) {
-        const allRoles = interaction.client.config.mainServer.roles;
-
         function sortMembers(a: GuildMember, b: GuildMember) {
             if (a.displayName.toLowerCase() < b.displayName.toLowerCase()) return -1;
             if (a.displayName.toLowerCase() > b.displayName.toLowerCase()) return 1;
@@ -12,33 +10,37 @@ export default {
         };
         
         ({
-            mp: async () => {
+            async mp() {
                 const staff = {
-                    mp_manager: interaction.guild.roles.cache.get(allRoles.mpmanager)?.members.sort(sortMembers),
-                    mp_sr_admin: interaction.guild.roles.cache.get(allRoles.mpsradmin)?.members.sort(sortMembers),
-                    mp_jr_admin: interaction.guild.roles.cache.get(allRoles.mpjradmin)?.members.sort(sortMembers),
-                    mp_farm_manager: interaction.guild.roles.cache.get(allRoles.mpfarmmanager)?.members.sort(sortMembers),
-                    mp_trusted_farmer: interaction.guild.roles.cache.get(allRoles.trustedfarmer)?.members.sort(sortMembers)
+                    mp_manager: interaction.client.getRole('mpmanager'),
+                    mp_sr_admin: interaction.client.getRole('mpsradmin'),
+                    mp_jr_admin: interaction.client.getRole('mpjradmin'),
+                    mp_farm_manager: interaction.client.getRole('mpfarmmanager'),
+                    mp_trusted_farmer: interaction.client.getRole('trustedfarmer')
                 };
          
                 interaction.reply({ embeds: [new EmbedBuilder()
                     .setTitle('__MP Staff Members__')
                     .setColor(interaction.client.config.embedColor)
                     .setDescription([
-                        `<@&${allRoles.mpmanager}>`,
-                        `${staff.mp_manager?.map(x => x.toString()).join("\n") || "None"}\n`,
-                        `<@&${allRoles.mpsradmin}>`,
-                        `${staff.mp_sr_admin?.map(x => x.toString()).join("\n") || "None"}\n`,
-                        `<@&${allRoles.mpjradmin}>`,
-                        `${staff.mp_jr_admin?.map(x => x.toString()).join("\n") || "None"}\n`,
-                        `<@&${allRoles.mpfarmmanager}>`,
-                        `${staff.mp_farm_manager?.map(x => x.toString()).join("\n") || "None"}\n`,
-                        `<@&${allRoles.trustedfarmer}>`,
-                        `${staff.mp_trusted_farmer?.map(x => x.toString()).join("\n") || "None"}`
+                        staff.mp_manager.toString(),
+                        staff.mp_manager.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None",
+                        '',
+                        staff.mp_sr_admin.toString(),
+                        staff.mp_sr_admin.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None",
+                        '',
+                        staff.mp_jr_admin.toString(),
+                        staff.mp_jr_admin.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None",
+                        '',
+                        staff.mp_farm_manager.toString(),
+                        staff.mp_farm_manager.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None",
+                        '',
+                        staff.mp_trusted_farmer.toString(),
+                        staff.mp_trusted_farmer.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None"
                     ].join('\n'))
                 ] });
             },
-            fs: () => {
+            async fs() {
                 interaction.reply({ embeds: [new EmbedBuilder()
                     .setTitle('__MP Staff Usernames__')
                     .setColor(interaction.client.config.embedColor)
@@ -48,38 +50,52 @@ export default {
                     )
                 ] });
             },
-            discord: async () => {
+            async discord() {
                 const staff = {
-                    admin: interaction.guild.roles.cache.get(allRoles.discordadmin)?.members,
-                    moderator: interaction.guild.roles.cache.get(allRoles.discordmoderator)?.members,
-                    helper: interaction.guild.roles.cache.get(allRoles.discordhelper)?.members
+                    admin: interaction.client.getRole('discordadmin'),
+                    moderator: interaction.client.getRole('discordmoderator'),
+                    helper: interaction.client.getRole('discordhelper')
                 };
          
                 interaction.reply({ embeds: [new EmbedBuilder()
                     .setTitle('__Discord Staff Members__')
                     .setColor(interaction.client.config.embedColor)
                     .setDescription([
-                        `<@&${allRoles.discordadmin}>`,
-                        `${staff.admin?.map(x => x.toString()).join("\n") || "None"}\n`,
-                        `<@&${allRoles.discordmoderator}>`,
-                        `${staff.moderator?.map(x => x.toString()).join("\n") || "None"}\n`,
-                        `<@&${allRoles.discordhelper}>`,
-                        `${staff.helper?.map(x => x.toString()).join("\n") || "None"}`
+                        staff.admin.toString(),
+                        staff.admin.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None",
+                        '',
+                        staff.moderator.toString(),
+                        staff.moderator.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None",
+                        '',
+                        staff.helper.toString(),
+                        staff.helper.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None"
                     ].join('\n'))
                 ] });
+            },
+            async mc() {
+                const staff = interaction.client.getRole('irtmcstaff');
+
+                interaction.reply({ embeds: [new EmbedBuilder()
+                    .setTitle('__IRTMC Staff Members__')
+                    .setColor(interaction.client.config.embedColor)
+                    .setDescription(`${staff.toString()}\n${staff.members.sort(sortMembers).map(x => x.toString()).join("\n") || "None"}`)
+                ] });
             }
-        } as any)[interaction.options.getSubcommand()]();
+        } as Index)[interaction.options.getSubcommand()]();
     },
     data: new SlashCommandBuilder()
         .setName("staff")
         .setDescription("Staff member information")
-        .addSubcommand(x=>x
+        .addSubcommand(x => x
             .setName("mp")
             .setDescription("Shows all MP Staff members within Discord"))
-        .addSubcommand(x=>x
+        .addSubcommand(x => x
             .setName("fs")
             .setDescription("Shows all MP Staff usernames within FS"))
-        .addSubcommand(x=>x
+        .addSubcommand(x => x
             .setName("discord")
             .setDescription("Shows all Discord staff members"))
+        .addSubcommand(x => x
+            .setName("mc")
+            .setDescription("Shows all IRTMC Staff members"))
 };

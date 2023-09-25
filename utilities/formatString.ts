5dd3aecd0c11222c47184423a782e96aae7e5227
TÆMBØ <kuneshr123@gmail.com>
2023-09-25 16:33:44 -0700

    Create formatString.ts

---
+++ utilities/formatString.ts
@@ -0,0 +1,18 @@
+/**
+ * Formats a string by making every first letter of each word (separated by a space) uppercase
+ * @param text The text to format
+ * @returns 
+ */
+export function formatString(text: string) {
+    text = text.trim();
+
+    if (text.includes(' ')) {
+        return text
+            .split(' ')
+            .map(x => x[0].toUpperCase() + x.slice(1))
+            .join(' ');
+    } else {
+        return text[0].toUpperCase() + text.slice(1);
+    }
+
+}

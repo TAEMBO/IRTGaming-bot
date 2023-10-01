e1c8d2d1523084d359cae459d861fd70f39e791d
TÆMBØ <kuneshr123@gmail.com>
2023-10-01 15:29:01 -0700

    Refine handling of persistent buttons

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -25,28 +25,32 @@ export default async (interaction: TClient<Discord.Interaction<"cached">>) => {
                 : interaction.reply(':warning: An error occurred while running this command');
         }
     } else if (interaction.isButton()) {
-        if (interaction.customId.startsWith('reaction-') && interaction.client.config.botSwitches.buttonRoles) { // Button roles
-            const roleId = interaction.customId.replace('reaction-', '');
+        const args = interaction.customId.split('-');
 
-            if (interaction.member.roles.cache.has(roleId)) {
-                interaction.member.roles.remove(roleId);
-                interaction.reply({ content: `You've been removed from <@&${roleId}>`, ephemeral: true });
-            } else {
-                interaction.member.roles.add(roleId);
-                interaction.reply({ content: `You've been added to <@&${roleId}>`, ephemeral: true });
-            }
-        } else if (interaction.customId.startsWith('sub-')) { // Subscriber role verification
-            const args = interaction.customId.replace('sub-', '').split('-');
+        ({
+            reaction() {
+                const roleId = args[1];
 
-            ({
-                yes() {
-                    interaction.guild.members.cache.get(args[1])?.roles.add(interaction.client.config.mainServer.roles.subscriber);
-                    interaction.message.edit({ content: interaction.message.content + '\n**Accepted verification**', components: [] });
-                },
-                no() {
-                    interaction.message.edit({ content: interaction.message.content + '\n**Denied verification**', components: [] });
+                if (interaction.member.roles.cache.has(roleId)) {
+                    interaction.member.roles.remove(roleId);
+                    interaction.reply({ content: `You've been removed from <@&${roleId}>`, ephemeral: true });
+                } else {
+                    interaction.member.roles.add(roleId);
+                    interaction.reply({ content: `You've been added to <@&${roleId}>`, ephemeral: true });
                 }
-            } as Index)[args[0]]();
-        } else log('Purple', `Alternate button pressed at ${interaction.message.url}`);
+            },
+            sub() {
+                ({
+                    yes() {
+                        interaction.guild.members.cache.get(args[2])?.roles.add(interaction.client.config.mainServer.roles.subscriber);
+                        interaction.message.edit({ content: interaction.message.content + '\n**Accepted verification**', components: [] });
+                    },
+                    no() {
+                        interaction.message.edit({ content: interaction.message.content + '\n**Denied verification**', components: [] });
+                    }
+                } as Index)[args[1]]();
+            }
+        } as Index)[args[0]]();
+
     }
 }

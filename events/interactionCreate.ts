d1d2d944b7bcf131e241ec05c5a5cb655563acf2
TÆMBØ <kuneshr123@gmail.com>
2023-10-01 22:43:53 -0700

    Improve command error handler

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -6,47 +6,56 @@ export default async (interaction: TClient<Discord.Interaction<"cached">>) => {
     if (!interaction.inGuild() || !interaction.inCachedGuild()) return;
 
     if (interaction.isChatInputCommand()) {
-        if (interaction.commandName === 'ping') return interaction.reply({ content: 'Pinging...', fetchReply: true }).then(msg => msg.edit(`Websocket: \`${interaction.client.ws.ping}\`ms\nBot: \`${msg.createdTimestamp - interaction.createdTimestamp}\`ms`));
+        if (interaction.commandName === 'ping') {
+            const msg = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+
+            return await msg.edit(`Websocket: \`${interaction.client.ws.ping}\`ms\nBot: \`${msg.createdTimestamp - interaction.createdTimestamp}\`ms`);
+        }
         
         const subCmd = interaction.options.getSubcommand(false);
         const command = interaction.client.commands.get(interaction.commandName) as Command;
 
         log('White', `\x1b[32m${interaction.user.tag}\x1b[37m used \x1b[32m/${interaction.commandName} ${subCmd ?? ''}\x1b[37m in \x1b[32m#${interaction.channel?.name}`);
-        if (!interaction.client.config.botSwitches.commands && !interaction.client.config.devWhitelist.includes(interaction.user.id)) return interaction.reply('Commands are currently disabled.');
+
+        if (!interaction.client.config.botSwitches.commands && !interaction.client.config.devWhitelist.includes(interaction.user.id)) return await interaction.reply('Commands are currently disabled.');
 
         try {
             await command.run(interaction);
             command.uses++;
         } catch (err) {
+            const errMsg = ":warning: An error occurred while running this command, bot developer(s) notified of matter";
+
             interaction.client.emit('intErr', err);
 
             interaction.replied
-                ? interaction.followUp(':warning: An error occurred while running this command')
-                : interaction.reply(':warning: An error occurred while running this command');
+                ? await interaction.followUp(errMsg) // Interaction replied to
+                : interaction.deferred
+                    ? await interaction.editReply(errMsg) // Interaction deferred
+                    : await interaction.reply(errMsg); // Interaction not replied to or deferred
         }
     } else if (interaction.isButton()) {
         const args = interaction.customId.split('-');
 
-        ({
-            reaction() {
+        await ({
+            async reaction() {
                 const roleId = args[1];
 
                 if (interaction.member.roles.cache.has(roleId)) {
-                    interaction.member.roles.remove(roleId);
-                    interaction.reply({ content: `You've been removed from <@&${roleId}>`, ephemeral: true });
+                    await interaction.member.roles.remove(roleId);
+                    await interaction.reply({ content: `You've been removed from <@&${roleId}>`, ephemeral: true });
                 } else {
-                    interaction.member.roles.add(roleId);
-                    interaction.reply({ content: `You've been added to <@&${roleId}>`, ephemeral: true });
+                    await interaction.member.roles.add(roleId);
+                    await interaction.reply({ content: `You've been added to <@&${roleId}>`, ephemeral: true });
                 }
             },
-            sub() {
-                ({
-                    yes() {
-                        interaction.guild.members.cache.get(args[2])?.roles.add(interaction.client.config.mainServer.roles.subscriber);
-                        interaction.message.edit({ content: interaction.message.content + '\n**Accepted verification**', components: [] });
+            async sub() {
+                await ({
+                    async yes() {
+                        await interaction.guild.members.cache.get(args[2])?.roles.add(interaction.client.config.mainServer.roles.subscriber);
+                        await interaction.message.edit({ content: interaction.message.content + '\n**Accepted verification**', components: [] });
                     },
-                    no() {
-                        interaction.message.edit({ content: interaction.message.content + '\n**Denied verification**', components: [] });
+                    async no() {
+                        await interaction.message.edit({ content: interaction.message.content + '\n**Denied verification**', components: [] });
                     }
                 } as Index)[args[1]]();
             }

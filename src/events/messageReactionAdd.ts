df162edd33d2d78d047621553a387f8928f38f1a
TÆMBØ <kuneshr123@gmail.com>
2024-02-12 21:33:55 -0800

    Add support for partial messages

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -1,4 +1,5 @@
 import type { MessageReaction, PartialMessageReaction, PartialUser, User } from "discord.js";
+import { tempReply } from "src/utils.js";
 
 export default async (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser) => {
     if (
@@ -6,6 +7,6 @@ export default async (reaction: MessageReaction | PartialMessageReaction, user: 
         || (reaction.message.author?.id === user.id && reaction.message.channelId === user.client.config.mainServer.channels.mfModSuggestions)
     ) {
         await reaction.users.remove(user.id);
-        await reaction.message.reply(`You cannot vote on your own suggestion, ${user}!`).then(msg => setTimeout(async () => await msg.delete(), 10_000));
+        await tempReply(reaction.message, { content: `You cannot vote on your own suggestion, ${user}!`, timeout: 10_000 });
     }
 };
--- src/util/tempReply.ts
+++ src/util/tempReply.ts
@@ -1,11 +1,11 @@
-import type { Message, MessageCreateOptions } from "discord.js";
+import type { Message, MessageCreateOptions, PartialMessage } from "discord.js";
 
 /**
  * Send a temporary message reply that deletes after a set timeout in milliseconds. Pings the user in the reply
  * @param message The message to temporarily reply to
  * @param options Options for sending the reply
  */
-export async function tempReply(message: Message, options: MessageCreateOptions & { timeout: number; }) {
+export async function tempReply(message: Message | PartialMessage, options: MessageCreateOptions & { timeout: number; }) {
     const msg = await message.reply({ ...options, allowedMentions: { repliedUser: true } });
 
     setTimeout(msg.delete.bind(msg), options.timeout);

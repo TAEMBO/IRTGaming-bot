1cc1f46389c19cda3a195923ce4cf7d8b341a3ab
TÆMBØ <kuneshr123@gmail.com>
2024-02-12 22:44:36 -0800

    Refactor displayment of permissions

--- src/commands/chatInput/roleinfo.ts
+++ src/commands/chatInput/roleinfo.ts
@@ -1,19 +1,34 @@
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { EmbedBuilder, PermissionFlagsBits, PermissionsBitField, SlashCommandBuilder } from "discord.js";
 import { Command } from "../../utils.js";
 
 export default new Command<"chatInput">({
     async run(interaction) {
         const role = interaction.options.getRole("role", true);
-        const keyPermissions = ["Administrator", "KickMembers", "BanMembers", "ManageChannels", "ManageGuild", "ViewAuditLog", "ManageMessages", "ManageNicknames", "MentionEveryone", "UseExternalEmojis", "ManageRoles", "ManageEmojisAndStickers", "ModerateMembers"];
-        const permissions = role.permissions.toArray();
-        const roleMembers = role.members.map(e =>`**${e.user.tag}**`).join("\n") || "";
-        const includedPermissions = permissions.includes("Administrator") ? ["Administrator"] : permissions.filter(x => keyPermissions.includes(x));
+        const keyPermissions = [
+            PermissionFlagsBits.Administrator,
+            PermissionFlagsBits.KickMembers,
+            PermissionFlagsBits.BanMembers,
+            PermissionFlagsBits.ManageChannels,
+            PermissionFlagsBits.ManageGuild,
+            PermissionFlagsBits.ViewAuditLog,
+            PermissionFlagsBits.ManageMessages,
+            PermissionFlagsBits.ManageNicknames,
+            PermissionFlagsBits.MentionEveryone,
+            PermissionFlagsBits.UseExternalEmojis,
+            PermissionFlagsBits.ManageRoles,
+            PermissionFlagsBits.ManageGuildExpressions,
+            PermissionFlagsBits.ModerateMembers
+        ];    
+        const roleMembers = role.members.map(member => `**${member.user.tag}**`).join("\n");
+        const includedPermissions = role.permissions.has(PermissionFlagsBits.Administrator)
+            ? [PermissionFlagsBits.Administrator]
+            : keyPermissions.filter(perm => role.permissions.has(perm, false));
 
         await interaction.reply({ embeds: [new EmbedBuilder()
             .setTitle(`Role Info: ${role.name}`)
             .addFields(
-                { name: "🔹 ID", value: `\`${role.id}\``, inline: true},
-                { name: "🔹 Color", value: `\`${role.hexColor}\``, inline: true},
+                { name: "🔹 ID", value: `\`${role.id}\``, inline: true },
+                { name: "🔹 Color", value: `\`${role.hexColor}\``, inline: true },
                 { name: "🔹 Created", value: `<t:${Math.round(role.createdTimestamp/1000)}:R> `, inline: true },
                 { name: "🔹 Misc", value: [
                     `Hoist: \`${role.hoist}\``,
@@ -21,7 +36,7 @@ export default new Command<"chatInput">({
                     `Position: \`${role.position}\` from bottom`,
                     `Members: \`${role.members.size}\`\n${role.members.size < 21 ? roleMembers : ""}`
                 ].join("\n"), inline: true },
-                { name: "🔹 Key Permissions", value: includedPermissions.join(", ") || "None", inline: true })
+                { name: "🔹 Key Permissions", value: new PermissionsBitField(includedPermissions).toArray().join(", ") || "None", inline: true })
             .setColor(role.color || "#ffffff")
             .setThumbnail(role.iconURL())
         ] });

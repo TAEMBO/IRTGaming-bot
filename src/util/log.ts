8a4ca5cb9a6ed56ce71c47275b70e82e6522d371
TÆMBØ <kuneshr123@gmail.com>
2024-03-26 21:27:47 -0700

    Reset log color after log statement

--- src/util/log.ts
+++ src/util/log.ts
@@ -17,5 +17,5 @@ enum LogColor {
  * @param data The data to be logged
  */
 export function log(color: keyof typeof LogColor, ...data: any[]) {
-    console.log(`${LogColor[color]}[${(new Date()).toLocaleString("en-GB")}]`, ...data);
+    console.log(`${LogColor[color]}[${(new Date()).toLocaleString("en-GB")}]`, ...data, LogColor.Reset);
 }

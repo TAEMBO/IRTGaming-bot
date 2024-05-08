0ae9427ea5f2221b46639109db79858a9094a4c6
TÆMBØ <kuneshr123@gmail.com>
2024-05-07 22:22:22 -0700

    Separate FSLoop utilities

--- src/util/index.ts
+++ src/util/index.ts
@@ -5,6 +5,7 @@ export * from "./formatString.js";
 export * from "./formatTime.js";
 export * from "./formatUser.js";
 export * from "./fsLoop.js";
+export * from "./fsLoopAll.js";
 export * from "./hasProfanity.js";
 export * from "./hasRole.js";
 export * from "./isDCStaff.js";

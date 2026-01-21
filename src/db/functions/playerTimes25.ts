b205fbf395db0c5fefa1648c9f89f1f43334611c
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2026-01-20 23:27:24 -0800

    Ignore player names of improper length

--- src/db/functions/playerTimes25.ts
+++ src/db/functions/playerTimes25.ts
@@ -1,11 +1,14 @@
 import { eq } from "drizzle-orm";
 import { db, playerTimes25Table } from "#db";
+import { log } from "#util";
 
 export function getTimeData(data: typeof playerTimes25Table.$inferSelect) {
     return Object.entries(data.servers).filter((x): x is [string, NonNullable<typeof x[1]>] => x[1] !== null);
 }
 
 export async function addPlayerTime(playerName: string, playerTime: number, serverAcro: string) {
+    if (playerName.length > 99 || playerName.length === 0) return log("yellow", `Skipping addPlayerTime due to length: "${playerName}"`);
+
     const now = Math.floor(Date.now() / 1_000);
     const playerTimesData = await getPlayerTimesRow(playerName);
 
